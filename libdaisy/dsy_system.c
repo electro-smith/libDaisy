4b6082bd822bede2f4c35f005dace5d2c9f64f0a
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-10-16 12:51:25 -0700

    added rudimentary adc support (only for daisy seed at the moment) and added SYSTICK handler to dsy_system

--- libdaisy/dsy_system.c
+++ libdaisy/dsy_system.c
@@ -50,6 +50,12 @@ typedef void(*EntryPoint)(void);
 static void SystemClock_Config(uint8_t board);
 static void Error_Handler(void);
 
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
 void dsy_system_init(uint8_t board)
 {
 	// For now we won't use the board parameter since all three supported boards use the same 16MHz HSE.
@@ -214,111 +220,6 @@ void SystemClock_Config(uint8_t board)
   /** Enable USB Voltage detector 
   */
   HAL_PWREx_EnableUSBVoltageDetector();
-}
-
-static void SystemClock_Config_old(void);
-// Static Function Definition
-static void SystemClock_Config_old(void)
-{
-
-	
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
-	RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = { 0 };
-
-	/*!< Supply configuration update enable */
-	MODIFY_REG(PWR->CR3, PWR_CR3_SCUEN, 0);
-
-	/** Supply configuration update enable 
-	*/
-	HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
-	/** Configure the main internal regulator output voltage 
-	*/
-	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-	while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-	/** Macro to configure the PLL clock source 
-	*/
-	__HAL_RCC_PLL_PLLSOURCE_CONFIG(RCC_PLLSOURCE_HSE);
-	/** Initializes the CPU, AHB and APB busses clocks 
-	*/
-	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI48 | RCC_OSCILLATORTYPE_HSE;
-	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-	RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
-	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-	RCC_OscInitStruct.PLL.PLLM = 4;
-	RCC_OscInitStruct.PLL.PLLN = 200; //400MHZ Sys Clk
-	//RCC_OscInitStruct.PLL.PLLN = 240; //400MHZ Sys Clk
-	RCC_OscInitStruct.PLL.PLLP = 2;
-	RCC_OscInitStruct.PLL.PLLQ = 3; // was 16
-	RCC_OscInitStruct.PLL.PLLR = 2;
-	RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
-	RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-	RCC_OscInitStruct.PLL.PLLFRACN = 0;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	/** Initializes the CPU, AHB and APB busses clocks 
-	*/
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-	                            | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
-	                            | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
-	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-	RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-	RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-	RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-	RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-	RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-	RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
-
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USART3 | RCC_PERIPHCLK_USART6
-	                            | RCC_PERIPHCLK_SPI1 | RCC_PERIPHCLK_SAI2
-	                            | RCC_PERIPHCLK_SAI1 | RCC_PERIPHCLK_SDMMC
-	                            | RCC_PERIPHCLK_I2C2 | RCC_PERIPHCLK_ADC
-	                            | RCC_PERIPHCLK_USB | RCC_PERIPHCLK_QSPI
-	                            | RCC_PERIPHCLK_FMC;
-	PeriphClkInitStruct.PLL2.PLL2M = 4;
-	PeriphClkInitStruct.PLL2.PLL2N = 100;
-	PeriphClkInitStruct.PLL2.PLL2P = 2;
-	PeriphClkInitStruct.PLL2.PLL2Q = 2;
-	PeriphClkInitStruct.PLL2.PLL2R = 2; // was 4
-	PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_2; // was 0
-	PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOWIDE;
-	PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
-	PeriphClkInitStruct.PLL3.PLL3M = 15;
-	PeriphClkInitStruct.PLL3.PLL3N = 368;
-	PeriphClkInitStruct.PLL3.PLL3P = 16;
-	PeriphClkInitStruct.PLL3.PLL3Q = 4; 
-	PeriphClkInitStruct.PLL3.PLL3R = 16; 
-	PeriphClkInitStruct.PLL3.PLL3RGE = RCC_PLL3VCIRANGE_0; // was 1
-	PeriphClkInitStruct.PLL3.PLL3VCOSEL = RCC_PLL3VCOWIDE;
-	PeriphClkInitStruct.PLL3.PLL3FRACN = 0;
-	//PeriphClkInitStruct.FmcClockSelection = RCC_FMCCLKSOURCE_D1HCLK;
-	PeriphClkInitStruct.FmcClockSelection = RCC_FMCCLKSOURCE_PLL;
-	PeriphClkInitStruct.QspiClockSelection = RCC_QSPICLKSOURCE_D1HCLK;
-	//PeriphClkInitStruct.QspiClockSelection = RCC_QSPICLKSOURCE_PLL2; // Updated to use PLL2 @ 120MHz
-	PeriphClkInitStruct.SdmmcClockSelection = RCC_SDMMCCLKSOURCE_PLL;
-	PeriphClkInitStruct.Sai1ClockSelection = RCC_SAI1CLKSOURCE_PLL3;
-	PeriphClkInitStruct.Sai23ClockSelection = RCC_SAI23CLKSOURCE_PLL3;
-	PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL;
-	PeriphClkInitStruct.Usart234578ClockSelection = RCC_USART234578CLKSOURCE_D2PCLK1;
-	PeriphClkInitStruct.Usart16ClockSelection = RCC_USART16CLKSOURCE_D2PCLK2;
-	PeriphClkInitStruct.I2c123ClockSelection = RCC_I2C123CLKSOURCE_PLL3;
-	PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
-	PeriphClkInitStruct.AdcClockSelection = RCC_ADCCLKSOURCE_PLL3;
-	if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
-	{
-		Error_Handler();
-	}
-	/** Enable USB Voltage detector 
-	*/
-	HAL_PWREx_EnableUSBVoltageDetector();
 }
 
 static void Error_Handler()
