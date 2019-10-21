b1db831f7be15cc515c64335c4088ced1ad367df
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-10-21 10:38:34 -0700

    added 24-bit capability to wm8731, barely tested.

--- libdaisy/codec_wm8731.h
+++ libdaisy/codec_wm8731.h
@@ -8,7 +8,8 @@
 
 uint8_t codec_wm8731_init(I2C_HandleTypeDef *hi2c,
 						  uint8_t			 mcu_is_master,
-						  int32_t			 sample_rate);
+						  int32_t			 sample_rate, 
+						  uint8_t			 bitdepth);
 uint8_t codec_wm8731_enter_bypass(I2C_HandleTypeDef *hi2c);
 uint8_t codec_wm8731_exit_bypass(I2C_HandleTypeDef *hi2c);
 
