bdc5cd33a48d8efa02682ea740b8a5d2ced9045a
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-10-28 13:45:42 -0700

    added a rebuild all script -- only outputs warnings/errors.

---
+++ rebuild_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+start_dir=$PWD
+
+echo "rebuilding libdaisy"
+cd libdaisy
+make clean | grep "warningr\|error"
+make | grep "warningr\|error"
+echo "done"
+
+echo "rebuilding daisysp"
+cd $start_dir
+cd daisysp
+make clean | grep "warningr\|error"
+make | grep "warningr\|error"
+cd $start_dir
+echo "done"
+
+echo "rebuilding examples:"
+for d in examples/*/; do
+    echo "rebuilding $d"
+    cd $d
+    make clean | grep "warning\|error" # grep for silencing make outputs when regenerating everything.
+    make | grep "warning\|error"
+    cd $start_dir
+    echo "done"
+done
+
+echo "finished"
+
