41fb68eb801897ca8c9ea4cf2b58f5612b9b4d41
aikenberry <andrew@qubitelectronix.com>
2019-11-20 21:30:47 -0800

    implemented missing functions in c file

--- daisysp/crossfade.c
+++ daisysp/crossfade.c
@@ -10,11 +10,29 @@ float dsy_crossfade_process(dsy_crossfade *p, float *in1, float *in2)
 {
 	switch (p->curve)
 	{
-		case DSY_CROSSFADE_LINEAR: 
+		case DSY_CROSSFADE_LIN: 
     		return ((*in2 * p->pos) + *in1) * (1 - p->pos);
+
+    	case DSY_CROSSFADE_CPOW:
+    		return 0;
+
+    	case DSY_CROSSFADE_LOG:
+    		return 0;
+
+    	case DSY_CROSSFADE_EXP:
+    		return 0;
 
     	default :
     		return 0;
-
 	}
+}
+
+void dsy_crossfade_set_pos(dsy_crossfade *p, float pos) 
+{
+	p->pos = pos;
+}
+
+void dsy_crossfade_set_curve(dsy_crossfade *p, uint8_t curve)
+{
+	p->curve = curve;
 }
