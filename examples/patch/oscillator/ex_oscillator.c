b8982412c0e53480b36a9e664a17f8e26443007d
aikenberry <andrew@qubitelectronix.com>
2019-11-21 18:45:06 -0800

    broke all examples by directory shakeup and created first pass of patch specific oscillator example

---
+++ examples/patch/oscillator/ex_oscillator.c
@@ -0,0 +1,51 @@
+#include "libdaisy.h"
+#include "daisysp.h"
+#include "dsy_patch_bsp.h"
+
+static daisy_patch patch;
+static dsy_oscillator_t osc;
+
+static void audioCallback(float *in, float *out, size_t size)
+{
+	float sig, freq, amp;
+	size_t wave;
+
+	freq = dsy_adc_get_float(DSY_PATCH_KNOB_1);
+	// scale signal to appropriate range / filter / etc.
+	osc->freq = freq;
+
+	wave = dsy_adc_get(DSY_PATCH_KNOB_2);
+	// scale signal to appropriate range / filter / etc.
+	osc->wave = wave;
+
+	amp = dsy_adc_get_float(DSY_PATCH_KNOB_3);
+	// scale signal to appropriate range / filter / etc.
+	osc->amp = amp;
+
+    for (size_t i = 0; i < size; i += 2)
+    {
+    	sig = dsy_oscillator_process(&osc);
+
+    	// left out
+        out[i] = sig;
+
+        // right out
+        out[i+1] = sig;
+    }
+}
+
+int main(void)
+{
+	// initialize patch hardware and oscillator daisysp module
+    daisy_seed_init(&patch.seed);
+    daisy_patch_init(&patch);
+    dsy_oscillator_init(&osc, DSY_AUDIO_SAMPLE_RATE);
+
+    // define callback
+    dsy_audio_set_callback(DSY_AUDIO_INTERNAL, audioCallback);
+
+    // start callback
+    dsy_audio_start(DSY_AUDIO_INTERNAL);
+
+    while(1) {}
+}
