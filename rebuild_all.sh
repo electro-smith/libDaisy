615269179c2742d71c8dffece1b342bb3dbf21a8
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-11-25 10:32:59 -0800

    fixed rebuild_all typo, and added field and petal template examples.

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -20,20 +20,7 @@ make | grep "warningr\|error"
 cd $start_dir
 echo "done"
 
-rebuild_examples.sh
-#echo "rebuilding examples:"
-#HW=hardware_platforms
-#example_dirs=(daisysp/examples $HW/field/examples $HW/patch/examples $HW/petal/examples $HW/pod/examples)
-#for e in ${example_dirs[@]}; do
-#    for d in $e/*/; do
-#        echo "rebuilding $d"
-#        cd $d
-#        make clean | grep "warning\|error" # grep for silencing make outputs when regenerating everything.
-#        make | grep "warning\|error"
-#        cd $start_dir
-#        echo "done"
-#    done
-#done
+./rebuild_examples.sh
 
 echo "finished"
 
