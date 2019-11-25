7b9b53c74603ebc85c2f28324687982a1e60e38c
aikenberry <andrewikenberry@gmail.com>
2019-11-25 11:55:25 -0800

    got it compiling

--- example_projects/patch_svf/ex_svf.c
+++ example_projects/patch_svf/ex_svf.c
@@ -4,23 +4,30 @@
 
 static daisy_patch patch;
 static dsy_svf_t filt;
-static dsy_crossfade crossfade;
 
 static void audioCallback(float *in, float *out, size_t size)
 {
     float cutoff, resonance, drive, in_amp;
     for (size_t i = 0; i < size; i += 2)
     {
-        verb.feedback = 0.15f + (dsy_adc_get_float(DSY_PATCH_KNOB_1) * 0.85f);
-
         // read controls
         cutoff = dsy_adc_get_float(DSY_PATCH_KNOB_1);
+        dsy_svf_set_fc(&filt, cutoff);
+
         resonance = dsy_adc_get_float(DSY_PATCH_KNOB_2);
+        dsy_svf_set_res(&filt, resonance);
+
         in_amp = dsy_adc_get_float(DSY_PATCH_KNOB_3);
+
         drive = dsy_adc_get_float(DSY_PATCH_KNOB_4);
+        dsy_svf_set_drive(&filt, drive);
+
+        out[i] = 
+
+        //out[i] = 
         
-        out[i] = (dryL * drylevel) + wetL;
-        out[i + 1] = (dryR * drylevel) + wetR;
+        //out[i] = (dryL * drylevel) + wetL;
+        //out[i + 1] = (dryR * drylevel) + wetR;
     }
 }
 
@@ -29,7 +36,7 @@ int main(void)
     // initialize seed/patch hardware and daisysp modules
     daisy_seed_init(&patch.seed);
     daisy_patch_init(&patch);
-    dsy_svf_t_init(&filt, DSY_AUDIO_SAMPLE_RATE);
+    dsy_svf_init(&filt, DSY_AUDIO_SAMPLE_RATE);
 
     // define callback
     dsy_audio_set_callback(DSY_AUDIO_INTERNAL, audioCallback);
