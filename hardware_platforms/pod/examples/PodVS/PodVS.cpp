31b0a9c00754ca43ad8d482d492a917869215fd2
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-11-26 17:08:06 -0800

    better stuff for knobs/cvs (need to rename file)

--- hardware_platforms/pod/examples/PodVS/PodVS.cpp
+++ hardware_platforms/pod/examples/PodVS/PodVS.cpp
@@ -1,34 +1,36 @@
-#include "daisy_pod.h"
+#include "daisy_patch.h"
 #include "daisysp.h"
 #include <math.h>
 
-const float freqmin = logf(20.0f);
-const float freqmax = logf(20000.0f);
-
-daisy_pod hw;
-dsy_knob k;
-dsy_switch *   button;
+daisy_patch	hw;
+dsy_ctrl	   k, cv;
 dsy_oscillator osc;
-uint8_t wf;
+uint8_t		   wf;
 
-static void audio(float *in, float *out, size_t size) 
+static float mtof(float m);
+
+static void audio(float *in, float *out, size_t size)
 {
 	float param, sig;
-	dsy_switch_debounce(button);
-	if(dsy_switch_falling_edge(button)) 
+	dsy_switch_debounce(&hw.button1);
+	if(dsy_switch_falling_edge(&hw.button1))
 	{
 		wf += 1;
-		if(wf > DSY_OSC_WAVE_LAST - 1) 
+		if(wf > DSY_OSC_WAVE_LAST - 1)
 		{
 			wf = 0;
 		}
 		osc.wave = (dsy_oscillator_waveform)wf;
 	}
-	for(size_t i = 0; i < size; i += 2) 
+	for(size_t i = 0; i < size; i += 2)
 	{
-		param = dsy_knob_process(&k);
-		osc.freq   = expf((param * (freqmax - freqmin)) + freqmin);
-		sig = dsy_oscillator_process(&osc);
+		param	  = dsy_ctrl_process(&k) * 72.0f; // 6 octave knob
+		param += dsy_ctrl_process(&cv) * 60.0f; // 5 octave CV (1V/Octave)
+		param = DSY_CLAMP(param, 0.0f, 127.0f); 
+		osc.freq = mtof(12.0f + param);
+
+		osc.amp	= 0.4f;
+		sig		   = dsy_oscillator_process(&osc);
 		out[i]	 = sig;
 		out[i + 1] = sig;
 	}
@@ -36,19 +38,18 @@ static void audio(float *in, float *out, size_t size)
 
 int main(void)
 {
-	daisy_pod_init(&hw);
-	button = &hw.switches[SW_1];
+	daisy_patch_init(&hw);
 	wf	 = 0;
-	dsy_knob_init(&k,
-				  dsy_adc_get_rawptr(KNOB_1),
-				  0.01f,
-				  DSY_KNOB_THRESH_LOCK,
-				  SAMPLE_RATE);
+	dsy_ctrl_init_knob(&k, dsy_adc_get_rawptr(KNOB_1), SAMPLE_RATE);
+	dsy_ctrl_init_bipolar_cv(&cv, dsy_adc_get_rawptr(CV_2), SAMPLE_RATE);
 	dsy_oscillator_init(&osc, SAMPLE_RATE);
 	dsy_audio_set_callback(DSY_AUDIO_INTERNAL, audio);
 	dsy_audio_start(DSY_AUDIO_INTERNAL);
 	dsy_adc_start();
-	while(1)
-	{
-	}
+	while(1) {}
+}
+
+static float mtof(float m) 
+{
+	return powf(2, (m - 69.0f) / 12.0f) * 440.0f;
 }
--- libdaisy/src/hid_knob.h
+++ libdaisy/src/hid_knob.h
@@ -6,30 +6,50 @@ extern "C"
 {
 #endif
 #include <stdint.h>
-	typedef enum
-	{
-		DSY_KNOB_THRESH_NO_LOCK,
-		DSY_KNOB_THRESH_LOCK,
-		DSY_KNOB_THRESH_LAST,
-	}dsy_knob_thresh;
-	typedef struct
-	{
-		uint16_t *raw;
-		float   coeff, thresh, sr, val, prev, delta;
-		uint8_t lock;
-	}dsy_knob;
+enum
+{
+	DSY_KNOB_THRESH_NO_LOCK,
+	DSY_KNOB_THRESH_LOCK,
+	DSY_KNOB_THRESH_LAST,
+};
+typedef struct
+{
+	uint16_t *raw;
+	float   coeff, thresh, sr, val, prev, delta;
+	float	 scale, offset;
+	uint8_t lock, flip;
+}dsy_ctrl;
 
-	// raw_ptr is a pointer to the adc's raw 16-bit buffer value. use dsy_adc_get_rawptr(chn)
-	// slewrate is the time in seconds it takes changes in the knob to reach their new position.
-	// change_thresh is the level at which a knob change will be detected, and unlock
-	// sr is used to configure the filters.
-	void dsy_knob_init(dsy_knob *p,
-					   uint16_t *rawptr,
-					   float	 slewrate,
-					   dsy_knob_thresh change_thresh, 
-					   float	 sr);
+// raw_ptr is a pointer to the adc's raw 16-bit buffer value. use dsy_adc_get_rawptr(chn)
+// slewrate is the time in seconds it takes changes in the ctrl to reach their new position.
+// change_thresh is the level at which a ctrl change will be detected, and unlock
+// sr is used to configure the filters.
+void dsy_ctrl_init(dsy_ctrl *p,
+				   uint16_t *rawptr,
+				   float	 scale,
+				   float	 offset,
+				   float	 slewrate,
+				   uint8_t   change_thresh,
+				   uint8_t   flip,
+				   float	 sr);
 
-	float dsy_knob_process(dsy_knob *p);
+
+// Initializes standard pot:
+// 0-3v3 -> 0-1.0
+// smoothing filter: 10ms smoothing
+// lock threshold at 0.001 movement detection
+void dsy_ctrl_init_knob(dsy_ctrl *p, uint16_t *rawptr, float sr);
+
+// Initializes eurorack bipolar CV:
+// -5V -> +5V to -1.0 -> 1.0
+// smoothing filter: 2ms smoothing
+// lock threshold at 0.002 movement detection
+// flipped 
+void dsy_ctrl_init_bipolar_cv(dsy_ctrl *p, uint16_t *rawptr, float sr);
+
+
+float dsy_ctrl_process(dsy_ctrl *p);
+
 #ifdef __cplusplus
 }
 #endif
