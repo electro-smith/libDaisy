e0eeebcb1c0ed4955262e03427e9e9e02550b033
aikenberry <andrew@qubitelectronix.com>
2019-11-30 15:14:49 -0800

    compiling with documentation

--- daisysp/modules/port.h
+++ daisysp/modules/port.h
@@ -1,11 +1,65 @@
-#ifndef DSY_PORT_H
-#define DSY_PORT_H
-typedef struct {
-    float htime;
-    float c1, c2, yt1, prvhtim;
-    float sr, onedsr;
-} dsy_port;
+// # port
+// Applies portamento to an input signal
+//
+// This code has been ported from Soundpipe to DaisySP by (paul batchelor?). 
+// The Soundpipe module was extracted from the Csound opcode "portk".
+// Original Author(s): Robbin Whittle, John ffitch
+// Year: 1995, 1998
+// Location: Opcodes/biquad.c
 
-void dsy_port_init(dsy_port *p, int sr, float htime);
-float dsy_port_compute(dsy_port *p, float in);
+#pragma once
+#ifndef PORT_H
+#define PORT_H
+#include <stdint.h>
+#ifdef __cplusplus
+
+namespace daisysp
+{
+	class port
+	{
+	public:
+		port() {}
+		~port() {}
+
+// ### init
+// Initializes port module
+// Arguments:
+// - sr: sample rate of audio engine
+// - htime: time of portamento
+
+// ~~~~
+		void init(int sr, float htime);
+// ~~~~
+
+// ### process
+// Applies portamento to input signal and returns processed signal. 
+
+// ~~~~
+		float process(float in);
+// ~~~~
+
+// ## Setters
+
+// ### set_htime
+// Sets htime
+
+// ~~~~
+		inline void set_htime(float htime) { _htime = htime; }
+// ~~~~
+
+// ## Getters
+
+// ### get_htime
+// returns current value of htime
+
+// ~~~~
+		inline float get_htime() { return _htime; }
+// ~~~~
+	private:
+		float _htime;
+    	float _c1, _c2, _yt1, _prvhtim;
+    	float _sr, _onedsr;
+	};
+} // namespace daisysp
+#endif
 #endif
