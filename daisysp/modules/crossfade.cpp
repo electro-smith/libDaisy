f9c5dab0c0700d0e22654b6c4e788c6dc4007e96
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-12-04 08:36:51 -0800

    fixed crossfade

--- daisysp/modules/crossfade.cpp
+++ daisysp/modules/crossfade.cpp
@@ -1,7 +1,12 @@
 #include <math.h>
 #include "crossfade.h"
 
+#define REALLYSMALLFLOAT 0.000001f
+
 using namespace daisysp;
+
+const float cross_log_min = logf(REALLYSMALLFLOAT);
+const float cross_log_max = logf(1.0f);
 
 float crossfade::process(float &in1, float &in2)
 {
@@ -10,7 +15,7 @@ float crossfade::process(float &in1, float &in2)
 	{
 		case CROSSFADE_LIN: 
             scalar_1 = pos_;
-    		return (*in1 * (1.0f - scalar_1)) + (*in2 * scalar_1);
+    		return (in1 * (1.0f - scalar_1)) + (in2 * scalar_1);
 
     	case CROSSFADE_CPOW:
             scalar_1 = sinf(pos_ * (M_PI * 0.5f));
@@ -18,12 +23,12 @@ float crossfade::process(float &in1, float &in2)
             return (in1 * scalar_2) + (in2 * scalar_1);
 
     	case CROSSFADE_LOG:
-            scalar_1 = expf(pos_ * (logf(1.0) - logf(REALLYSMALLFLOAT)) + logf(REALLYSMALLFLOAT));
-    		return (*in1 * (1.0f - scalar_1)) + (*in2 * scalar_1);
+            scalar_1 = expf(pos_ * (cross_log_max - cross_log_min) + cross_log_min);
+    		return (in1 * (1.0f - scalar_1)) + (in2 * scalar_1);
 
     	case CROSSFADE_EXP:
             scalar_1 = pos_ * pos_;
-    		return (*in1 * (1.0f - scalar_1)) + (*in2 * scalar_1);
+    		return (in1 * (1.0f - scalar_1)) + (in2 * scalar_1);
 
     	default :
     		return 0;
