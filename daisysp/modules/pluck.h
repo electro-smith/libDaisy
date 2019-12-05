49df4ed7aa779a58d3fbeb8acfc09c61c52f295b
aikenberry <andrew@qubitelectronix.com>
2019-12-05 06:48:41 -0800

    added documentation and cleaned up code

--- daisysp/modules/pluck.h
+++ daisysp/modules/pluck.h
@@ -1,5 +1,7 @@
 //
-// pluck
+// # pluck
+//
+// Produces a naturally decaying plucked string or drum sound based on the Karplus-Strong algorithms.
 // 
 // This code has been extracted from the Csound opcode "pluck"
 // It has been modified to work as a Daisy Soundpipe module.
@@ -20,11 +22,19 @@
 
 namespace daisysp
 {
+
+// ## Mode
+// The method of natural decay that the algorithm will use.
+//
+// - RECURSIVE: 1st order recursive filter, with coefs .5.
+// - WEIGHTED_AVERAGE: weighted averaging. 
+// ~~~~
 	enum 
 	{
 		PLUCK_MODE_RECURSIVE,
 		PLUCK_MODE_WEIGHTED_AVERAGE,
 	};
+// ~~~~
 
 	class pluck
 	{
@@ -32,20 +42,101 @@ namespace daisysp
 		pluck() {}
 		~pluck() {}
 
+// ### init
+// 
+// Initializes the Pluck module.
+//
+// Arguments:
+// 
+// - sample_rate: Sample rate of the audio engine being run.
+// - buf: Buffer for ?
+// - npt: 
+// - mode: Sets the mode of the algorithm.
+// 
+// ~~~~
 		void init (float sample_rate, float *buf, int32_t npt, int32_t mode);
-		float process (float *trig);
+// ~~~~
 
-		// Setters
+// ### process
+//
+// Processes the waveform to be generated, returning one sample. This should be called once per sample period.
+// ~~~~
+		float process (float &trig);
+// ~~~~
+
+// ## Mutators
+// 
+// ### set_amp
+// 
+// Sets the amplitude of the output signal.
+// 
+// Input range: 0-1?
+// 
+// ~~~~
 		inline void set_amp(float amp) { amp_ = amp; }
-		inline void set_freq(float freq) { freq_ = freq; }
-		inline void set_decay(float decay) { decay_ = decay; }
-		inline void set_mode(int32_t mode) { mode_ = mode; }
+// ~~~~
 
-		// Getters
+// ### set_freq
+// 
+// Sets the frequency of the output signal in Hz.
+// 
+// Input range: Any positive value
+// 
+// ~~~~
+		inline void set_freq(float freq) { freq_ = freq; }
+// ~~~~
+
+// ### set_decay
+// 
+// Sets the time it takes for a triggered note to end in seconds.
+// 
+// Input range: Any positive value
+// 
+// ~~~~
+		inline void set_decay(float decay) { decay_ = decay; }
+// ~~~~
+
+// ### set_mode
+// 
+// Sets the mode of the algorithm.
+// 
+// ~~~~
+		inline void set_mode(int32_t mode) { mode_ = mode; }
+// ~~~~
+
+// ## Accessors
+// 
+// ### get_amp
+// 
+// Returns the current value for amp.
+// 
+// ~~~~
 		inline float get_amp() { return amp_; }
+// ~~~~
+
+// ### get_freq
+// 
+// Returns the current value for freq.
+// 
+// ~~~~
 		inline float get_freq() { return freq_; }
+// ~~~~
+
+// ### get_decay
+// 
+// Returns the current value for decay.
+// 
+// ~~~~
 		inline float get_decay() { return decay_; }
+// ~~~~
+
+// ### get_mode
+// 
+// Returns the current value for mode.
+// 
+// ~~~~
 		inline int32_t get_mode() { return mode_; }
+// ~~~~
 
 	private:
 		void reinit();
