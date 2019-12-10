d2af34fb175e46c4541850f86a17bf2876a128dd
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-12-10 09:37:07 -0800

    added hardware oversampling (configurable) to adc periph. changed clock speed for ADCs in system

--- libdaisy/src/per_adc.h
+++ libdaisy/src/per_adc.h
@@ -38,10 +38,26 @@ extern "C"
 		MUX_SEL_LAST,
 	} dsy_adc_mux_pin;
 
+	typedef enum
+	{
+		DSY_ADC_OVS_NONE,
+		DSY_ADC_OVS_4,
+		DSY_ADC_OVS_8,
+		DSY_ADC_OVS_16,
+		DSY_ADC_OVS_32,
+		DSY_ADC_OVS_64,
+		DSY_ADC_OVS_128,
+		DSY_ADC_OVS_256,
+		DSY_ADC_OVS_512,
+		DSY_ADC_OVS_1024,
+		DSY_ADC_OVS_LAST,
+	} dsy_adc_oversampling;
+
 	typedef struct
 	{
 		dsy_gpio_pin pin_config[DSY_ADC_PIN_LAST];
 		dsy_gpio_pin mux_pin_config[DSY_ADC_PIN_LAST][MUX_SEL_LAST];
+		dsy_adc_oversampling oversampling;
 		uint8_t		 active_channels[DSY_ADC_PIN_LAST];
 		uint8_t		 channels, mux_channels[DSY_ADC_PIN_LAST];
 	} dsy_adc_handle;
