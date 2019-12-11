44150b564627122e6da251d8937893cab5d5826b
aikenberry <andrewikenberry@gmail.com>
2019-12-11 07:47:39 -0800

    first pass at crossfade example. Compiling fine, need to test on hardware

---
+++ daisysp/examples/crossfade/ex_crossfade.cpp
@@ -0,0 +1,63 @@
+#include "daisysp.h"
+#include "daisy_seed.h"
+
+using namespace daisysp;
+
+static daisy_handle seed;
+static crossfade cfade;
+static oscillator osc_sine;
+static oscillator osc_saw;
+static phasor phsr;
+
+static void audioCallback(float *in, float *out, size_t size)
+{
+	float saw, sine, pos, output;
+    for (size_t i = 0; i < size; i += 2)
+    {
+    	sine = osc_sine.process();
+        saw = osc_saw.process();
+        pos = phsr.process();
+        cfade.set_pos(pos);
+
+        output = cfade.process(sine, saw);
+
+    	// left out
+        out[i] = output;
+
+        // right out
+        out[i+1] = output;
+    }
+}
+
+int main(void)
+{
+	// initialize seed hardware and daisysp modules
+    daisy_seed_init(&seed);
+
+    // set params for crossfade object
+    cfade.init();
+    cfade.set_curve(CROSSFADE_LIN);
+
+    // set parameters for sine oscillator object
+    osc_sine.init(DSY_AUDIO_SAMPLE_RATE);
+    osc_sine.set_waveform(osc_sine.WAVE_SIN);
+    osc_sine.set_freq(440);
+    osc_sine.set_amp(0.5);
+
+    // set parameters for sawtooth oscillator object
+    osc_saw.init(DSY_AUDIO_SAMPLE_RATE);
+    osc_saw.set_waveform(osc_saw.WAVE_SAW);
+    osc_saw.set_freq(440);
+    osc_saw.set_amp(0.5);
+
+    // set params for phasor object
+    phsr.init(DSY_AUDIO_SAMPLE_RATE, .5);
+
+    // define callback
+    dsy_audio_set_callback(DSY_AUDIO_INTERNAL, audioCallback);
+
+    // start callback
+    dsy_audio_start(DSY_AUDIO_INTERNAL);
+
+    while(1) {}
+}
