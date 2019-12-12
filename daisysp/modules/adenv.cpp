2cca91fba36b32ad2827ff96bc9e995db325092a
aikenberry <andrewikenberry@gmail.com>
2019-12-11 16:26:55 -0800

    implemented getter function for current segment and renamed member variables to trailing underscores

--- daisysp/modules/adenv.cpp
+++ daisysp/modules/adenv.cpp
@@ -6,66 +6,66 @@ using namespace daisysp;
 // Private Functions
 void adenv::init(float sample_rate)
 {
-	_sr = sample_rate;
-	_current_segment = ADENV_SEG_IDLE;
-	_curve_scalar = 0.0f;  // full linear
-	_phase = 0;
-	_multiplier = 1.0f;
-	//__phase_inc = ((2.0f * PI * (1.0f / _segment_time[ADENV_SEG_ATTACK]) / SAMPLE_RATE));// / 2.0f);
-	_phase_inc = 1.0f;
-	_min = 0.0f;
-	_max = 1.0f;
-	_output = 0.0001f;
+	sample_rate_ = sample_rate;
+	current_segment_ = ADENV_SEG_IDLE;
+	curve_scalar_ = 0.0f;  // full linear
+	phase_ = 0;
+	multiplier_ = 1.0f;
+	//phase__inc_ = ((2.0f * PI * (1.0f / segment_time_[ADENV_SEG_ATTACK]) / SAMPLE_RATE));// / 2.0f);
+	phase_inc_ = 1.0f;
+	min_ = 0.0f;
+	max_ = 1.0f;
+	output_ = 0.0001f;
 	for (uint8_t i = 0; i < ADENV_SEG_LAST; i++) {
-		_segment_time[i] = 0.05f;
+		segment_time_[i] = 0.05f;
 	}	
 }
 
 float adenv::process()
 {
 	uint32_t time_samps;
-	if (_trigger)
+	if (trigger_)
 	{
-		_trigger = 0;
-		_current_segment = ADENV_SEG_ATTACK;
-		_phase = 0;
-        time_samps = (uint32_t)(_segment_time[_current_segment] * _sr);
-        calculate_multiplier(_output, 1.0f, time_samps);
+		trigger_ = 0;
+		current_segment_ = ADENV_SEG_ATTACK;
+		phase_ = 0;
+        time_samps = (uint32_t)(segment_time_[current_segment_] * sample_rate_);
+        calculate_multiplier(output_, 1.0f, time_samps);
 	}
     else
     {
-        time_samps = (uint32_t)(_segment_time[_current_segment] * _sr);
+        time_samps = (uint32_t)(segment_time_[current_segment_] * sample_rate_);
     }
-	if (_phase >= time_samps)
+	if (phase_ >= time_samps)
 	{
-		switch (_current_segment)	
+		switch (current_segment_)	
 		{
 		case ADENV_SEG_ATTACK:
-			_current_segment = ADENV_SEG_DECAY;
-			time_samps = (uint32_t)(_segment_time[_current_segment] * _sr);
-			_phase = 0;
-			_output = 1.0f;
+			current_segment_ = ADENV_SEG_DECAY;
+			time_samps = (uint32_t)(segment_time_[current_segment_] * sample_rate_);
+			phase_ = 0;
+			output_ = 1.0f;
 			calculate_multiplier(1.0f, 0.0001f, time_samps);
 			break;
 		case ADENV_SEG_DECAY:
-			_current_segment = ADENV_SEG_IDLE;
-			_phase = 0;
-			_multiplier = 1.0f;
-			_output = 0.0f;
+			current_segment_ = ADENV_SEG_IDLE;
+			phase_ = 0;
+			multiplier_ = 1.0f;
+			output_ = 0.0f;
 			break;
 		default:
-			_multiplier = 0.0f;		
-			_output = 0.0f;
-			_phase = 0;
+			multiplier_ = 0.0f;		
+			output_ = 0.0f;
+			phase_ = 0;
 			break;
 		}
 	}
-	_output *= _multiplier;
-	_phase += 1;
-	return _output * (_max - _min) + _min;
+	output_ *= multiplier_;
+	phase_ += 1;
+	return output_ * (max_ - min_) + min_;
 }
 
 void adenv::calculate_multiplier(float start, float end, uint32_t length_in_samples) 
 {
-	_multiplier = 1.0f + ((end - start) / (float)length_in_samples);
+	multiplier_ = 1.0f + ((end - start) / (float)length_in_samples);
 }
