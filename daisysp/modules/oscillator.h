f9ca13c55933c60809cf41e1fad63e25ebabb55a
Stephen Hensley <stephen.p.hensley@gmail.com>
2019-12-19 14:07:20 -0800

    added examples to the documentation, and fixed oscillator/oscillator_markup so that there's only one.

--- daisysp/modules/oscillator.h
+++ daisysp/modules/oscillator.h
@@ -3,6 +3,30 @@
 #define DSY_OSCILLATOR_H
 #include <stdint.h>
 #ifdef __cplusplus
+// # oscillator
+// 
+//      Synthesis of several waveforms, including polyBLEP bandlimited waveforms.
+//
+// example:
+//
+// ~~~~
+// daisysp::oscillator osc;
+// init()
+// {
+//     osc.init(SAMPLE_RATE);
+//     osc.set_frequency(440);
+//     osc.set_amp(0.25);
+//     osc.set_waveform(WAVE_TRI);
+// }
+//
+// callback(float *in, float *out, size_t size)
+// {
+//     for (size_t i = 0; i < size; i+=2)
+//     {
+//         out[i] = out[i+1] = osc.process();
+//     }
+// }
+// ~~~~
 namespace daisysp
 {
 	class oscillator
@@ -11,6 +35,10 @@ namespace daisysp
 		oscillator() {}
 		~oscillator() {}
 
+// ## Waveforms
+//
+// Choices for output waveforms, POLYBLEP are appropriately labeled. Others are naive forms.
+// ~~~~
 		enum
 		{
 			WAVE_SIN,	
@@ -23,8 +51,22 @@ namespace daisysp
 			WAVE_POLYBLEP_SQUARE,
 			WAVE_LAST,
 		};
+// ~~~~
 
+// ### init
+//
+// Initializes the oscillator 
+//
+// float samplerate - sample rate of the audio engine being run, and the frequency that the process function will be called.
+//
+// Defaults:
+// - freq = 100 Hz
+// - amp = 0.5 
+// - waveform = sine wave.
+//
+// ~~~~
 		void init(float samplerate)
+// ~~~~
 		{
 			sr		  = samplerate;
 			freq	  = 100.0f;
@@ -34,17 +76,37 @@ namespace daisysp
 			waveform  = WAVE_SIN;
 		}
 
+// ### set_freq
+//
+// Changes the frequency of the oscillator, and recalculates phase increment.
+// ~~~~
 		inline void set_freq(const float f) 
+// ~~~~
 		{
 			freq	  = f;
 			phase_inc = calc_phase_inc(f);
 		}
 
+// ### set_amp
+//
+// Sets the amplitude of the waveform.
+// ~~~~
 		inline void set_amp(const float a) { amp = a; }
+// ~~~~
 
+// ### set_waveform
+//
+// Sets the waveform to be synthesized by the process() function.
+// ~~~~
 		inline void set_waveform(const uint8_t wf) { waveform = wf < WAVE_LAST ? wf : WAVE_SIN; }
+// ~~~~
 
+// ### process
+//
+// Processes the waveform to be generated, returning one sample. This should be called once per sample period.
+// ~~~~
 		float process();
+// ~~~~
 
 	  private:
 		float   calc_phase_inc(float f);
--- rebuild_docs.sh
+++ rebuild_docs.sh
@@ -9,9 +9,20 @@ echo "creating daisysp markdown files. . . "
 for header in $daisysp_dir/modules/*.h; do
     fname=${header##*/}
     fname=${fname%.h}
+    ex=$daisysp_dir/examples/$fname/ex_$fname.cpp
     outfile=$daisysp_dir/doc/$fname.md
     echo "    "$fname
     python makedoc.py $header > $outfile
+    # add example to the end of each doc.
+    echo $'\n## Example\n' >> $outfile
+    if [ -f "$ex" ]; then
+        echo $'```C++\n' >> $outfile
+        cat $ex >> $outfile
+        echo $'\n```' >> $outfile
+    else
+        echo "No example Provided for $fname"
+        echo "No example Provided" >> $outfile
+    fi
 done
 echo "done."
 
