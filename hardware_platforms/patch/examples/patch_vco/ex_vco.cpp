6c74d3624d281ccf4aee143efe69d01c26c06872
aikenberry <andrewikenberry@gmail.com>
2019-12-20 15:11:01 -0800

    updated param inits to be correct range and removed scaling math from proc functions

--- hardware_platforms/patch/examples/patch_vco/ex_vco.cpp
+++ hardware_platforms/patch/examples/patch_vco/ex_vco.cpp
@@ -15,15 +15,18 @@ static void audioCallback(float *in, float *out, size_t size)
     for (size_t i = 0; i < size; i += 2)
     {
         // Read Knobs
-        freq = 10.0f + (knob1.process() * 2000.0f); // scaled to 2kHz
-        wave = knob2.process() * (osc.WAVE_LAST - 1);
+        freq = knob1.process(); 
+        wave = knob2.process();
         amp = knob3.process();
+
         // Set osc params
         osc.set_freq(freq);
         osc.set_waveform(wave);
         osc.set_amp(amp);
+
         // process
     	sig = osc.process();
+        
     	// left out
         out[i] = sig;
         // right out
@@ -35,8 +38,8 @@ int main(void)
 {
     patch.init(); // initialize hardware (daisy seed, and patch)
     osc.init(SAMPLE_RATE); // init oscillator
-    knob1.init(patch.ctrl(KNOB_1), 0.0, 1.0, PARAM_CURVE_LINEAR);
-    knob2.init(patch.ctrl(KNOB_2), 0.0, 1.0, PARAM_CURVE_LINEAR);
+    knob1.init(patch.ctrl(KNOB_1), 10, 2000, PARAM_CURVE_LINEAR);
+    knob2.init(patch.ctrl(KNOB_2), 1, 4, PARAM_CURVE_LINEAR);
     knob3.init(patch.ctrl(KNOB_3), 0.0, 1.0, PARAM_CURVE_LINEAR);
     dsy_audio_set_callback(DSY_AUDIO_INTERNAL, audioCallback); // assign callback
     dsy_adc_start(); // start the ADCs to read values in the background.
