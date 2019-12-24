b067fab42b0b5c4bf50fe8072cfad1cd1d5eeaf2
aikenberry <andrewikenberry@gmail.com>
2019-12-24 09:58:53 -0800

    rough outline for complete patch vco example

--- hardware_platforms/patch/examples/patch_vco/ex_vco.cpp
+++ hardware_platforms/patch/examples/patch_vco/ex_vco.cpp
@@ -1,3 +1,11 @@
+// patch vco
+// knob1 = coarse
+// knob2 = fine
+// knob3 = waveform
+// knob4 = fm index
+// toggle = naive or polyblep waveforms (TODO)
+// bottom row of LEDs = current waveform (TODO)
+// TODO (other) implement CVs
 #include "daisysp.h"
 #include "daisy_patch.h"
 
@@ -6,24 +14,36 @@ using namespace daisysp;
 
 daisy_patch patch;
 oscillator osc;
-parameter freqctrl, wavectrl, ampctrl;
+parameter coarse_param, wave_param, fine_param, index_param;
+Switch wave_toggle;
 
 static void AudioCallback(float *in, float *out, size_t size)
 {
-	float sig, freq, amp;
+	float freq, sig, index;
 	size_t wave;
+    bool wave_type;
+
+    // read controls
+    freq = mtof(coarse_param.process());
+    freq  += mtof(fine_param.process());
+    index = index_param.process();
+    wave = wave_param.process();
+    wave_type = wave_toggle.Pressed();
+    wave_toggle.Debounce();
+
+    // Set osc params
+    osc.set_waveform(wave);
+
+    // audio buffer
     for (size_t i = 0; i < size; i += 2)
     {
-        // Read Knobs
-        freq = mtof(freqctrl.process());
-        wave = wavectrl.process();
-        amp = ampctrl.process();
-        // Set osc params
+        // read FM input, scale by index and add to freq
+        freq += in[i] * index;
         osc.set_freq(freq);
-        osc.set_waveform(wave);
-        osc.set_amp(amp);
+
         // process
     	sig = osc.process();
+
     	// left out
         out[i] = sig;
         // right out
@@ -33,19 +53,21 @@ static void AudioCallback(float *in, float *out, size_t size)
 
 int main(void)
 {
-    int num_waves = oscillator::WAVE_LAST - 1;
+    //int num_waves = oscillator::WAVE_LAST - 1;
+
+    // initialize objects
     patch.Init(); // initialize hardware (daisy seed, and patch)
     osc.init(SAMPLE_RATE); // init oscillator
+    osc.set_amp(.25);
+    wave_toggle.Init(TOGGLE_PIN, 100);
 
-    // This is with the GetCtrl, but it can also be done with the public members.
-    //freqctrl.init(patch.GetCtrl(daisy_patch::KNOB_1), 10.0, 110.0, parameter::LINEAR);
-    //wavectrl.init(patch.GetCtrl(daisy_patch::KNOB_2), 0.0, num_waves, parameter::LINEAR);
-    //ampctrl.init(patch.GetCtrl(daisy_patch::KNOB_2), 0.0, 0.5, parameter::LINEAR);
-    // Like this:
-    freqctrl.init(patch.knob1, 10.0, 110.0f, parameter::LINEAR);
-    wavectrl.init(patch.knob2, 0.0, num_waves, parameter::LINEAR);
-    ampctrl.init(patch.knob3, 0.0, 0.5f, parameter::LINEAR);
+    // initialize controls
+    coarse_param.init(patch.knob1, 10.0f, 110.0f, parameter::LINEAR); // coarse frequency
+    wave_param.init(patch.knob2, 0.0, 4.0f, parameter::LINEAR); // waveform
+    fine_param.init(patch.knob3, 0.0, 0.5f, parameter::LINEAR); // fine frequency
+    index_param.init(patch.knob4, 0.0, 100.0f, parameter::LINEAR); // FM index
 
+    // start adc and audio
     dsy_adc_start();
     patch.StartAudio(AudioCallback);
 
