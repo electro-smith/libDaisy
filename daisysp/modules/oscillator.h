da8ec6445787fb481515b670b80bbc81bbec0bbd
Dmitry Bezrodnykh <mayamorok@gmail.com>
2020-01-03 16:34:46 -0800

    Working on oscillator until I hit an undefined reference error with the linker (from ex_oscillator.cpp) in the function Oscillator::CalcPhaseInc(float f).

--- daisysp/modules/oscillator.h
+++ daisysp/modules/oscillator.h
@@ -3,37 +3,17 @@
 #define DSY_OSCILLATOR_H
 #include <stdint.h>
 #ifdef __cplusplus
-// # oscillator
+// # Oscillator
 // 
 //      Synthesis of several waveforms, including polyBLEP bandlimited waveforms.
 //
-// example:
-//
-// ~~~~
-// daisysp::oscillator osc;
-// init()
-// {
-//     osc.init(SAMPLE_RATE);
-//     osc.set_frequency(440);
-//     osc.set_amp(0.25);
-//     osc.set_waveform(WAVE_TRI);
-// }
-//
-// callback(float *in, float *out, size_t size)
-// {
-//     for (size_t i = 0; i < size; i+=2)
-//     {
-//         out[i] = out[i+1] = osc.process();
-//     }
-// }
-// ~~~~
 namespace daisysp
 {
-	class oscillator
+	class Oscillator
 	{
 	  public:
-		oscillator() {}
-		~oscillator() {}
+		Oscillator() {}
+		~Oscillator() {}
 
 // ## Waveforms
 //
@@ -53,67 +33,67 @@ namespace daisysp
 		};
 // ~~~~
 
-// ### init
+// ### Init
 //
-// Initializes the oscillator 
+// Initializes the Oscillator 
 //
-// float sample_rate - sample rate of the audio engine being run, and the frequency that the process function will be called.
+// float sample_rate - sample rate of the audio engine being run, and the frequency that the Process function will be called.
 //
 // Defaults:
-// - freq = 100 Hz
-// - amp = 0.5 
-// - waveform = sine wave.
+// - freq_ = 100 Hz
+// - amp_ = 0.5 
+// - waveform_ = sine wave.
 //
 // ~~~~
-		void init(float sample_rate)
+		void Init(float sample_rate)
 // ~~~~
 		{
-			sr		  = sample_rate;
-			freq	  = 100.0f;
-			amp		  = 0.5f;
-			phase	 = 0.0f;
-			phase_inc = calc_phase_inc(freq);
-			waveform  = WAVE_SIN;
+			sr_		  = sample_rate;
+			freq_	  = 100.0f;
+			amp_		  = 0.5f;
+			phase_	 = 0.0f;
+			phase_inc_ = CalcPhaseInc(freq_);
+			waveform_  = WAVE_SIN;
 		}
 
-// ### set_freq
+// ### SetFreq
 //
-// Changes the frequency of the oscillator, and recalculates phase increment.
+// Changes the frequency of the Oscillator, and recalculates phase increment.
 // ~~~~
-		inline void set_freq(const float f) 
+		inline void SetFreq(const float f) 
 // ~~~~
 		{
-			freq	  = f;
-			phase_inc = calc_phase_inc(f);
+			freq_	  = f;
+			phase_inc_ = CalcPhaseInc(f);
 		}
 
-// ### set_amp
+// ### SetAmp
 //
 // Sets the amplitude of the waveform.
 // ~~~~
-		inline void set_amp(const float a) { amp = a; }
+		inline void SetAmp(const float a) { amp_ = a; }
 // ~~~~
 
-// ### set_waveform
+// ### SetWaveform
 //
-// Sets the waveform to be synthesized by the process() function.
+// Sets the waveform to be synthesized by the Process() function.
 // ~~~~
-		inline void set_waveform(const uint8_t wf) { waveform = wf < WAVE_LAST ? wf : WAVE_SIN; }
+		inline void SetWaveform(const uint8_t wf) { waveform_ = wf < WAVE_LAST ? wf : WAVE_SIN; }
 // ~~~~
 
-// ### process
+// ### Process
 //
 // Processes the waveform to be generated, returning one sample. This should be called once per sample period.
 // ~~~~
-		float process();
+		float Process();
 // ~~~~
 
 	  private:
-		float   calc_phase_inc(float f);
-		uint8_t waveform;
-		float   amp, freq;
-		float   sr, phase, phase_inc;
-		float   last_out, last_freq;
+		float   CalcPhaseInc(float f);
+		uint8_t waveform_;
+		float   amp_, freq_;
+		float   sr_, phase_, phase_inc_;
+		float   last_out_, last_freq_;
 	};
 } // namespace daisysp
 #endif
