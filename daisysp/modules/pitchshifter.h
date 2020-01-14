6c66f8cb16566e783a9ed85ff5d41e7c241afea3
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-01-14 09:16:24 -0800

    fleshed out the 'fun' control on the pitchshifter.

--- daisysp/modules/pitchshifter.h
+++ daisysp/modules/pitchshifter.h
@@ -18,6 +18,20 @@
 #include "delayline.h"
 #include "phasor.h"
 
+static inline uint32_t hash_xs32(uint32_t x)
+{
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    return x;
+}
+inline uint32_t myrand()
+{
+    static uint32_t seed = 1;
+    seed                 = hash_xs32(seed);
+    return seed;
+}
+
 // Shift can be 30-100 ms lets just start with 50 for now.
 // 0.050 * SR = 2400 samples (at 48kHz)
 //#define SHIFT_BUFFER_SIZE 4800
@@ -27,129 +41,144 @@
 
 namespace daisysp
 {
-	class pitchshifter
-	{
-	  public:
-		pitchshifter() {}
-		~pitchshifter() {}
+class pitchshifter
+{
+  public:
+    pitchshifter() {}
+    ~pitchshifter() {}
 
-		void init(float sr)
-		{
-            force_recalc_ = false;
-            sr_		  = sr;
-			mod_freq_ = 5.0f;
-			set_semitones();
-			for(uint8_t i = 0; i < 2; i++)
-			{
-				gain_[i] = 0.0f;
-				d_[i].init();
-				phs_[i].init(sr, 50, i == 0 ? 0 : (float)M_PI);
-			}
-			shift_up_ = true;
-            del_size_ = SHIFT_BUFFER_SIZE;
-            set_del_size(del_size_);
-            fun_ = 0.0f;
-        }
-        float process(float &in)
+    void init(float sr)
+    {
+        force_recalc_ = false;
+        sr_           = sr;
+        mod_freq_     = 5.0f;
+        set_semitones();
+        for(uint8_t i = 0; i < 2; i++)
         {
-			float val, fade1, fade2;
-			// First process delay mod/crossfade
-			fade1 = phs_[0].process();
-			fade2 = phs_[1].process();
-			if(shift_up_)
-			{
-				fade1 = 1.0f - fade1;
-				fade2 = 1.0f - fade2;
-			}
-            mod_[0] = fade1 * (del_size_ - 1);
-            mod_[1] = fade2 * (del_size_ - 1);
+            gain_[i] = 0.0f;
+            d_[i].init();
+            phs_[i].init(sr, 50, i == 0 ? 0 : (float)M_PI);
+        }
+        shift_up_ = true;
+        del_size_ = SHIFT_BUFFER_SIZE;
+        set_del_size(del_size_);
+        fun_ = 0.0f;
+    }
+    float process(float &in)
+    {
+        float val, fade1, fade2;
+        // First process delay mod/crossfade
+        fade1 = phs_[0].process();
+        fade2 = phs_[1].process();
+        if(prev_phs_a_ > fade1)
+        {
+            mod_a_amt_ = fun_ * ((float)(myrand() % 255) / 255.0f)
+                         * (del_size_ * 0.5f);
+			mod_coeff_[0] = 0.0002f + (((float)(myrand() % 255) / 255.0f) * 0.001f);
+        }
+        if(prev_phs_b_ > fade2)
+        {
+            mod_b_amt_ = fun_ * ((float)(myrand() % 255) / 255.0f)
+                         * (del_size_ * 0.5f);
+			mod_coeff_[1] = 0.0002f + (((float)(myrand() % 255) / 255.0f) * 0.001f);
+        }
+        slewed_mod_[0] += mod_coeff_[0] * (mod_a_amt_ - slewed_mod_[0]);
+        slewed_mod_[1] += mod_coeff_[1] * (mod_b_amt_ - slewed_mod_[1]);
+        prev_phs_a_ = fade1;
+        prev_phs_b_ = fade2;
+        if(shift_up_)
+        {
+            fade1 = 1.0f - fade1;
+            fade2 = 1.0f - fade2;
+        }
+        mod_[0] = fade1 * (del_size_ - 1);
+        mod_[1] = fade2 * (del_size_ - 1);
 #ifdef USE_ARM_DSP
-            gain_[0] = arm_sin_f32(fade1 * (float)M_PI);
-            gain_[1] = arm_sin_f32(fade2 * (float)M_PI);
+        gain_[0] = arm_sin_f32(fade1 * (float)M_PI);
+        gain_[1] = arm_sin_f32(fade2 * (float)M_PI);
 #else
-			gain_[0] = sinf(fade1 * (float)M_PI);
-			gain_[1] = sinf(fade2 * (float)M_PI);
+        gain_[0] = sinf(fade1 * (float)M_PI);
+        gain_[1] = sinf(fade2 * (float)M_PI);
 #endif
 
-			// Handle Delay Writing
-			d_[0].write(in);
-			d_[1].write(in);
-            // Modulate Delay Lines
-            float mod_a_amt, mod_b_amt;
-            mod_a_amt = mod_b_amt = 0.0f;
-            //            mod_a_amt
-            //                = fun_ * ((float)(rand() % 255) / 255.0f) * (del_size_ * 0.01f);
-            //            mod_b_amt
-            //                = fun_ * ((float)(rand() % 255) / 255.0f) * (del_size_ * 0.01f);
-            d_[0].set_delay(mod_[0] + mod_a_amt);
-            d_[1].set_delay(mod_[1] + mod_b_amt);
-            val = 0.0f;
-			val += (d_[0].read() * gain_[0]);
-			val += (d_[1].read() * gain_[1]);
-			return val;
-		}
-		void set_transposition(const float &transpose)
-		{
-			float   ratio;
-			uint8_t idx;
-			if(transpose_ != transpose || force_recalc_)
-			{
-				transpose_ = transpose;
-				idx		   = fabsf(transpose);
-				ratio	  = semitone_ratios_[idx % 12];
-				ratio *= (uint8_t)(fabsf(transpose) / 12) + 1;
-				if(transpose > 0.0f)
-				{
-					shift_up_ = true;
-				}
-				else
-				{
-					shift_up_ = false;
-                }
-				mod_freq_ = ((ratio - 1.0f) * sr_) / del_size_;
-				if(mod_freq_ < 0.0f)
-				{
-					mod_freq_ = 0.0f;
-				}
-				phs_[0].set_freq(mod_freq_);
-				phs_[1].set_freq(mod_freq_);
-                if(force_recalc_) {
-                    force_recalc_ = false;
-                }
+        // Handle Delay Writing
+        d_[0].write(in);
+        d_[1].write(in);
+        // Modulate Delay Lines
+        //            mod_a_amt = mod_b_amt = 0.0f;
+//        d_[0].set_delay(mod_[0] + mod_a_amt_);
+//        d_[1].set_delay(mod_[1] + mod_b_amt_);
+        d_[0].set_delay(mod_[0] + slewed_mod_[0]);
+        d_[1].set_delay(mod_[1] + slewed_mod_[1]);
+        val = 0.0f;
+        val += (d_[0].read() * gain_[0]);
+        val += (d_[1].read() * gain_[1]);
+        return val;
+    }
+    void set_transposition(const float &transpose)
+    {
+        float   ratio;
+        uint8_t idx;
+        if(transpose_ != transpose || force_recalc_)
+        {
+            transpose_ = transpose;
+            idx        = fabsf(transpose);
+            ratio      = semitone_ratios_[idx % 12];
+            ratio *= (uint8_t)(fabsf(transpose) / 12) + 1;
+            if(transpose > 0.0f)
+            {
+                shift_up_ = true;
+            }
+            else
+            {
+                shift_up_ = false;
+            }
+            mod_freq_ = ((ratio - 1.0f) * sr_) / del_size_;
+            if(mod_freq_ < 0.0f)
+            {
+                mod_freq_ = 0.0f;
+            }
+            phs_[0].set_freq(mod_freq_);
+            phs_[1].set_freq(mod_freq_);
+            if(force_recalc_)
+            {
+                force_recalc_ = false;
             }
         }
+    }
 
-        void set_del_size(uint32_t size)
+    void set_del_size(uint32_t size)
+    {
+        del_size_     = size < SHIFT_BUFFER_SIZE ? size : SHIFT_BUFFER_SIZE;
+        force_recalc_ = true;
+        set_transposition(transpose_);
+    }
+
+    inline void set_fun(float f) { fun_ = f; }
+
+  private:
+    inline void set_semitones()
+    {
+        for(size_t i = 0; i < 12; i++)
         {
-            del_size_ = size < SHIFT_BUFFER_SIZE ? size : SHIFT_BUFFER_SIZE;
-            force_recalc_ = true;
-            set_transposition(transpose_);
+            semitone_ratios_[i] = powf(2.0f, (float)i / 12);
         }
-
-        inline void set_fun(float f) { fun_ = f; }
-
-      private:
-        inline void set_semitones()
-        {
-			for(size_t i = 0; i < 12; i++)
-			{
-				semitone_ratios_[i] = powf(2.0f, (float)i / 12);
-			}
-		}
-		typedef delayline<float, SHIFT_BUFFER_SIZE> shift_delay;
-		shift_delay									d_[2];
-        float                                       pitch_shift_, mod_freq_;
-        uint32_t                                    del_size_;
-        // lfo stuff
-        bool   force_recalc_;
-        float  sr_;
-        bool   shift_up_;
-        phasor phs_[2];
-        float  gain_[2], mod_[2], transpose_;
-        float  fun_;
-        // pitch stuff
-        float semitone_ratios_[12];
-    };
-    } // namespace daisysp
+    }
+    typedef delayline<float, SHIFT_BUFFER_SIZE> shift_delay;
+    shift_delay                                 d_[2];
+    float                                       pitch_shift_, mod_freq_;
+    uint32_t                                    del_size_;
+    // lfo stuff
+    bool   force_recalc_;
+    float  sr_;
+    bool   shift_up_;
+    phasor phs_[2];
+    float  gain_[2], mod_[2], transpose_;
+    float  fun_, mod_a_amt_, mod_b_amt_, prev_phs_a_, prev_phs_b_;
+    float  slewed_mod_[2], mod_coeff_[2];
+    // pitch stuff
+    float semitone_ratios_[12];
+};
+} // namespace daisysp
 
 #endif
