dd805960c974bc8e0be10a17ec5b239fce83a533
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-03-16 11:47:14 -0700

    updated oled stuff with char and string writing. added fonts

--- libdaisy/src/hid_oled_display.cpp
+++ libdaisy/src/hid_oled_display.cpp
@@ -11,9 +11,68 @@
 // RES -> PB8 (I2C1_SDA)
 // DC -> PB9 (I2C1_SCL)
 
+// Commands
+//
+// For now only the single-byte commands are notated.
+// Some others are here with info on what the other bytes are.
+// A few of the more complicated or mask-based commands have
+// not been populated yet.
+//
+// Fundamental Commands (SSD1309 datasheet page 27)
+//
+// Requires 8-bit data for contrast 1-256
+#define SSD1309_CMD_SET_CONTRAST (0x81)
+#define SSD1309_CMD_ENTIRE_DISPLAY_ON (0xa4)
+#define SSD1309_CMD_SET_NORMAL_DISPLAY (0xa6)
+#define SSD1309_CMD_SET_INVERSE_DISPLAY (0xa7)
+#define SSD1309_CMD_SET_DISPLAY_OFF (0xae)
+#define SSD1309_CMD_SET_DISPLAY_ON (0xaf)
+#define SSD1309_CMD_SET_NOP (0xe3)
+
+// TODO: Add notation for using this.
+#define SSD1309_CMD_SET_COMMAND_LOCK (0xfd)
+
+// Scrolling Commands (added as needed, some may still be missing (SSD1309 datasheett page 29)
+
+// Addressing Setting Commands (added as needed, some may still be missing (SSD1309 datasheett page 29)
+
+// takes a byte after from lsb to msb {horiontal, vertical, page, invalid}
+#define SSD1309_CMD_SET_MEMORY_ADDRESSING_MODE (0x20)
+
+// Hardware Configuration
+#define SSD1309_CMD_SET_COM_OUTPUT_SCAN_DIR_NORMAL (0xc0)
+#define SSD1309_CMD_SET_COM_OUTPUT_SCAN_DIR_REMAPPED (0xc8)
+#define SSD1309_CMD_SET_SEGMENT_REMAP_NORMAL (0xa0)
+#define SSD1309_CMD_SET_SEGMENT_REMAP_MIRROR (0xa1)
+// below commands require 1-64 as 0x00 to 0x3f byte following.
+#define SSD1309_CMD_SET_MUX_RATIO (0xa8)
+#define SSD1309_CMD_SET_DISPLAY_OFFSET (0xd3)
+
+// See page 33 of datasheet for usage of below (resets to 0x12)
+#define SSD1309_CMD_SET_COM_PIN_HW_CONFIG (0xda)
+
+// Timing and Driving Scheme Setting Commands
+
+// The following requires a byte with the following data to follow:
+// [7:4] Osc frequency && [3:0] Divide ratio as ([3:0] + 1)
+#define SSD1309_CMD_SET_CLOCK_DIVIDE_RATIO (0xd5)
+
+// The following requires a byte with the following data to follow:
+// [7:4] Phase 2 period of up to 15 DCLK (resets to 0x02)
+// [3:0] Phase 1 period of up to 15 DCLK (resets to 0x02)
+#define SSD1309_CMD_SET_PRECHARGE_PERIOD (0xd9)
+
+// The following nibble should be at [5:2]
+// Hex codes for whole bytes:
+// 0x00 - ~0.64 x Vcc
+// 0x34 - ~0.78 x Vcc (default @ RESET)
+// 0x3c - ~0.84 x Vcc
+#define SSD1309_CMD_SET_VCOMH_DESEL_LVL (0xdb)
+
+
 using namespace daisy;
 
-static uint8_t SSD1306_Buffer[SSD1306_WIDTH * SSD1306_HEIGHT / 8];
+static uint8_t SSD1309_Buffer[SSD1309_WIDTH * SSD1309_HEIGHT / 8];
 
 typedef struct
 {
@@ -21,16 +80,13 @@ typedef struct
     uint16_t CurrentY;
     uint8_t  Inverted;
     uint8_t  Initialized;
-} SSD1306_t;
+} SSD1309_t;
 static SpiHandle    h_spi;
 static dsy_gpio     pin_reset, pin_dc;
-static dsy_gpio_pin cfg_reset = {DSY_GPIOB, 8};
-static dsy_gpio_pin cfg_dc    = {DSY_GPIOB, 9};
+static dsy_gpio_pin cfg_reset = {DSY_GPIOB, 9};
+static dsy_gpio_pin cfg_dc    = {DSY_GPIOB, 8};
 
-static void SendCommand(uint8_t byte);
-static void SendData(uint8_t* buff, size_t size);
-
-static SSD1306_t SSD1306;
+static SSD1309_t SSD1309;
 
 
 void OledDisplay::Init()
@@ -39,67 +95,67 @@ void OledDisplay::Init()
     pin_reset.mode = DSY_GPIO_MODE_OUTPUT_PP;
     pin_reset.pin  = cfg_reset;
     dsy_gpio_init(&pin_reset);
-    pin_dc.mode    = DSY_GPIO_MODE_OUTPUT_PP;
-    pin_dc.pin     = cfg_dc;
+    pin_dc.mode = DSY_GPIO_MODE_OUTPUT_PP;
+    pin_dc.pin  = cfg_dc;
     dsy_gpio_init(&pin_dc);
+    h_spi.Init();
     Reset();
-    SendCommand(0xAE); // display off
-    SendCommand(0x20); // Set Memory Addressing Mode
-    SendCommand(
-        0x00); // 00b Horizontal, 01b Vertical, 10b Page Address (RESET), 11b Invalid
-    SendCommand(0xB0); // Set Page Start Address for Page Addressing Mode 0-7
-    SendCommand(0xc8); // Set COM Output Scan Direction
+    //SendCommand(0xAE); // display off
+    SendCommand(SSD1309_CMD_SET_DISPLAY_OFF);
+    // Set Memory Addressing Mode to Horizontal
+    SendCommand(SSD1309_CMD_SET_MEMORY_ADDRESSING_MODE);
+    SendCommand(0x00);
+    // Set Page Start Address (Page 0) -- only valid in Page Addressing Mode.
+    SendCommand(0xB0);
+    // Set COM Output Scan Direction
+    SendCommand(SSD1309_CMD_SET_COM_OUTPUT_SCAN_DIR_REMAPPED);
+    // Set Low and then High Column Address nibbles
     SendCommand(0x00); // set low common address
     SendCommand(0x10); // set high column address
-    SendCommand(0x40); //--set start line address - CHECK
+    // Set Display RAM dispaly start line register from 0-63
+    // start line register resets to 0b000000 on RESET.
+    SendCommand(0x40);
+    //--set contrast control register - CHECK
+    SendCommand(SSD1309_CMD_SET_CONTRAST);
+    SendCommand(0xFF); // 255
 
-    SendCommand(0x81); //--set contrast control register - CHECK
-    SendCommand(0xFF);
+    SendCommand(SSD1309_CMD_SET_SEGMENT_REMAP_MIRROR); // previously 'MIRROR'
+    SendCommand(SSD1309_CMD_SET_NORMAL_DISPLAY);
 
-#ifdef SSD1306_MIRROR_HORIZ
-    SendCommand(0xA0); // Mirror horizontally
-#else
-    SendCommand(0xA1); //--set segment re-map 0 to 127 - CHECK
-#endif
+    //Set Multiplex Ratio 1-64 (3f = 64)
+    SendCommand(SSD1309_CMD_SET_MUX_RATIO);
+    SendCommand(0x3F);
 
-#ifdef SSD1306_INVERSE_COLOR
-    SendCommand(0xA7); //--set inverse color
-#else
-    SendCommand(0xA6); //--set normal color
-#endif
+    SendCommand(SSD1309_CMD_ENTIRE_DISPLAY_ON);
 
-    SendCommand(0xA8); //--set multiplex ratio(1 to 64) - CHECK
-    SendCommand(0x3F); //
+    SendCommand(SSD1309_CMD_SET_DISPLAY_OFFSET); //-set display offset - CHECK
+    SendCommand(0x00);                           //-not offset
+
+    SendCommand(SSD1309_CMD_SET_CLOCK_DIVIDE_RATIO);
+    SendCommand(0xF0); // (reset value is 0x70)
+
+    SendCommand(SSD1309_CMD_SET_PRECHARGE_PERIOD);
+    SendCommand(0x22); // defaults after reset
 
     SendCommand(
-        0xA4); //0xa4,Output follows RAM content;0xa5,Output ignores RAM content
-
-    SendCommand(0xD3); //-set display offset - CHECK
-    SendCommand(0x00); //-not offset
-
-    SendCommand(
-        0xD5); //--set display clock divide ratio/oscillator frequency
-    SendCommand(0xF0); //--set divide ratio
-
-    SendCommand(0xD9); //--set pre-charge period
-    SendCommand(0x22); //
-
-    SendCommand(0xDA); //--set com pins hardware configuration - CHECK
+        SSD1309_CMD_SET_COM_PIN_HW_CONFIG); //--set com pins hardware configuration - CHECK
     SendCommand(0x12);
 
-    SendCommand(0xDB); //--set vcomh
-    SendCommand(0x20); //0x20,0.77xVcc
+    SendCommand(SSD1309_CMD_SET_VCOMH_DESEL_LVL); //--set vcomh
+    SendCommand(0x20);                            //0x20,0.77xVcc
 
-    SendCommand(0x8D); //--set DC-DC enable
-    SendCommand(0x14); //
-    SendCommand(0xAF); //--turn on SSD1306 panel
+    // Not sure if these two commands do anything on the SSD1309...
+    //    SendCommand(0x8D); //--set DC-DC enable
+    //    SendCommand(0x14); //
+
+    SendCommand(SSD1309_CMD_SET_DISPLAY_ON); //--turn on SSD1309 panel
     Fill(false);
     Update();
     // Set default values for screen object
-    SSD1306.CurrentX = 0;
-    SSD1306.CurrentY = 0;
+    SSD1309.CurrentX = 0;
+    SSD1309.CurrentY = 0;
 
-    SSD1306.Initialized = 1;
+    SSD1309.Initialized = 1;
 }
 void OledDisplay::Reset()
 {
@@ -111,8 +167,9 @@ void OledDisplay::Reset()
 
 void OledDisplay::Fill(bool on)
 {
-    for(size_t i = 0; i < sizeof(SSD1306_Buffer); i++) {
-        SSD1306_Buffer[i] = on ? 0xff : 0x00;
+    for(size_t i = 0; i < sizeof(SSD1309_Buffer); i++)
+    {
+        SSD1309_Buffer[i] = on ? 0xff : 0x00;
     }
 }
 
@@ -124,16 +181,91 @@ void OledDisplay::Update(void)
         SendCommand(0xB0 + i);
         SendCommand(0x00);
         SendCommand(0x10);
-        SendData(&SSD1306_Buffer[SSD1306_WIDTH * i], SSD1306_WIDTH);
+        SendData(&SSD1309_Buffer[SSD1309_WIDTH * i], SSD1309_WIDTH);
     }
 }
- 
-static void SendCommand(uint8_t byte)
+
+void OledDisplay::DrawPixel(uint8_t x, uint8_t y, bool on)
+{
+    if(x >= SSD1309_WIDTH || y >= SSD1309_HEIGHT)
+        return;
+    if(SSD1309.Inverted)
+        on = !on;
+    if(on)
+        SSD1309_Buffer[x + (y / 8) * SSD1309_WIDTH] |= (1 << (y % 8));
+    else
+        SSD1309_Buffer[x + (y / 8) * SSD1309_WIDTH] &= ~(1 << (y % 8));
+}
+char OledDisplay::WriteChar(char ch, FontDef font, bool on)
+{
+    uint32_t i, b, j;
+
+    // Check if character is valid
+    if(ch < 32 || ch > 126)
+        return 0;
+
+    // Check remaining space on current line
+    if(SSD1309_WIDTH < (SSD1309.CurrentX + font.FontWidth)
+       || SSD1309_HEIGHT < (SSD1309.CurrentY + font.FontHeight))
+    {
+        // Not enough space on current line
+        return 0;
+    }
+
+    // Use the font to write
+    for(i = 0; i < font.FontHeight; i++)
+    {
+        b = font.data[(ch - 32) * font.FontHeight + i];
+        for(j = 0; j < font.FontWidth; j++)
+        {
+            if((b << j) & 0x8000)
+            {
+                DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), on);
+            }
+            else
+            {
+                DrawPixel(SSD1309.CurrentX + j, (SSD1309.CurrentY + i), !on);
+            }
+        }
+    }
+
+    // The current space is now taken
+    SSD1309.CurrentX += font.FontWidth;
+
+    // Return written char for validation
+    return ch;
+}
+char OledDisplay::WriteString(char* str, FontDef font, bool on)
+{
+    // Write until null-byte
+    while(*str)
+    {
+        if(WriteChar(*str, font, on) != *str)
+        {
+            // Char could not be written
+            return *str;
+        }
+
+        // Next char
+        str++;
+    }
+
+    // Everything ok
+    return *str;
+}
+void OledDisplay::SetCursor(uint8_t x, uint8_t y)
+{
+    SSD1309.CurrentX = x;
+    SSD1309.CurrentY = y;
+}
+
+
+void OledDisplay::SendCommand(uint8_t byte)
 {
     dsy_gpio_write(&pin_dc, 0);
     h_spi.BlockingTransmit(&byte, 1);
 }
-static void SendData(uint8_t* buff, size_t size)
+void OledDisplay::SendData(uint8_t* buff, size_t size)
 {
     dsy_gpio_write(&pin_dc, 1);
     h_spi.BlockingTransmit(buff, size);
--- libdaisy/src/hid_oled_display.h
+++ libdaisy/src/hid_oled_display.h
@@ -1,16 +1,20 @@
 #pragma once
 #ifndef DSY_OLED_DISPLAY_H
 #define DSY_OLED_DISPLAY_H
+#include <stdlib.h>
+#include <stdint.h>
+#include "util_oled_fonts.h"
 
 // Credit to akiskon/stm32-ssd1306 on github for a great starting point.
 // For now Im hard coding everything, and then I'll move configurations out.
-#ifndef SSD1306_HEIGHT
-#define SSD1306_HEIGHT 64
+
+#ifndef SSD1309_HEIGHT
+#define SSD1309_HEIGHT 64
 #endif
 
-// SSD1306 width in pixels
-#ifndef SSD1306_WIDTH
-#define SSD1306_WIDTH 128
+// SSD1309 width in pixels
+#ifndef SSD1309_WIDTH
+#define SSD1309_WIDTH 128
 #endif
 
 namespace daisy
@@ -21,12 +25,17 @@ class OledDisplay
     OledDisplay() {}
     ~OledDisplay() {}
     void Init();
-
-    void Reset();
     void Fill(bool on);
+    void DrawPixel(uint8_t x, uint8_t y, bool on);
+    char WriteChar(char ch, FontDef font, bool on);
+    char WriteString(char* str, FontDef font, bool on);
+    void SetCursor(uint8_t x, uint8_t y);
     void Update();
 
   private:
+    void Reset();
+    void SendCommand(uint8_t byte);
+    void SendData(uint8_t *buff, size_t size);
 };
 } // namespace daisy
 
