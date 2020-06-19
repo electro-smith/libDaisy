6f4f930c3a1a087dbec94db58414bc6e01dc68d4
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-06-19 13:49:14 -0700

    added mechanism for handling overrun to keep program from getting stuck in UART callback

--- src/per_uart.cpp
+++ src/per_uart.cpp
@@ -120,7 +120,10 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef* huart)
     {
         // TODO:
         // Add handling for non-writable, overflow conditions, etc.
-        uhandle.queue_rx.Write(uhandle.dma_buffer_rx[i]);
+		if (uhandle.queue_rx.writable())
+			uhandle.queue_rx.Write(uhandle.dma_buffer_rx[i]);
+        else
+            uhandle.queue_rx.Overwrite(uhandle.dma_buffer_rx[i]);
     }
 }
 
