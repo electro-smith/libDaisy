78fda64101f7b138f5bddeee3ff50acbb0a1d465
Stephen Hensley <stephen.p.hensley@gmail.com>
2020-07-09 12:18:38 -0700

    updated daisy field for new hardware and added to makefile

---
+++ src/daisy_field.cpp
@@ -0,0 +1,197 @@
+#include "daisy_field.h"
+#ifndef SAMPLE_RATE
+#define SAMPLE_RATE DSY_AUDIO_SAMPLE_RATE /**< & */
+#endif
+
+//// Rev1 Pins
+//// Hardware related defines.
+//#define SW_1_PIN 29    /**< & */
+//#define SW_2_PIN 28    /**< & */
+//#define SW_3_PIN 27    /**< & */
+//#define GATE_OUT_PIN 0 /**< & */
+//#define GATE_IN_PIN 1  /**< & */
+//
+//// keyboard switches shift register
+//#define KB_SW_SR_CS_PIN 8  /**< & */
+//#define KB_SW_SR_CLK_PIN 9 /**< & */
+//#define KB_SW_SR_D1_PIN 10 /**< & */
+//#define KB_SW_SR_D2_PIN 11 /**< & */
+//
+//// UART for MIDI via TRS jacks on Field
+//#define MIDI_OUT_PIN 14 /**< & */
+//#define MIDI_IN_PIN 15  /**< & */
+//
+////CD4051 Select Pin controls
+//#define MUX_SEL_0_PIN 21 /**< & */
+//#define MUX_SEL_1_PIN 20 /**< & */
+//#define MUX_SEL_2_PIN 19 /**< & */
+//
+//#define MUX_ADC_PIN 16 /**< & */
+//#define CV1_ADC_PIN 17 /**< & */
+//#define CV2_ADC_PIN 18 /**< & */
+//#define CV3_ADC_PIN 23 /**< & */
+//#define CV4_ADC_PIN 22 /**< & */
+
+// Rev2 Pins
+#define PIN_GATE_IN 0
+#define PIN_SPI_CS 7
+#define PIN_SPI_SCK 8
+#define PIN_OLED_CMD 9
+#define PIN_SPI_MOSI 10
+#define PIN_I2C_SCL 11
+#define PIN_I2C_SDA 12
+#define PIN_MIDI_OUT 13
+#define PIN_MIDI_IN 14
+#define PIN_GATE_OUT 15
+#define PIN_ADC_POT_MUX 16
+#define PIN_ADC_CV_1 17
+#define PIN_ADC_CV_2 18
+#define PIN_MUX_SEL_2 19
+#define PIN_MUX_SEL_1 20
+#define PIN_MUX_SEL_0 21
+#define PIN_CD4021_D1 26
+#define PIN_CD4021_CS 27
+#define PIN_CD4021_CLK 28
+#define PIN_SW_2 29
+#define PIN_SW_1 30
+
+// IT LOOKS LIKE THESE MAY NEED TO GET SWAPPED.....
+#define PIN_ADC_CV_4 22
+#define PIN_ADC_CV_3 23
+#define PIN_DAC_2 24
+#define PIN_DAC_1 25
+
+#define LED_DRIVER_I2C i2c1_handle /**< & */
+
+using namespace daisy;
+
+void DaisyField::Init()
+{
+    seed.Configure();
+    seed.Init();
+    samplerate_ = seed.AudioSampleRate();
+    blocksize_  = 48;
+    seed.SetAudioBlockSize(blocksize_);
+    blockrate_ = samplerate_ / (float)blocksize_;
+
+    // Switches
+    uint8_t sw_pin[]  = {PIN_SW_1, PIN_SW_2};
+    uint8_t adc_pin[] = {PIN_ADC_CV_1,
+                         PIN_ADC_CV_2,
+                         PIN_ADC_CV_3,
+                         PIN_ADC_CV_4,
+                         PIN_ADC_POT_MUX};
+
+    for(size_t i = 0; i < SW_LAST; i++)
+    {
+        dsy_gpio_pin p = seed.GetPin(sw_pin[i]);
+        sw_[i].Init(p, blockrate_);
+    }
+
+    // ADCs
+    AdcChannelConfig adc_cfg[CV_LAST + 1];
+    for(size_t i = 0; i < CV_LAST; i++)
+    {
+        adc_cfg[i].InitSingle(seed.GetPin(adc_pin[i]));
+    }
+    // POT MUX
+    adc_cfg[CV_LAST].InitMux(seed.GetPin(PIN_ADC_POT_MUX),
+                             8,
+                             seed.GetPin(PIN_MUX_SEL_0),
+                             seed.GetPin(PIN_MUX_SEL_1),
+                             seed.GetPin(PIN_MUX_SEL_2));
+    seed.adc.Init(adc_cfg, 5);
+
+    for(size_t i = 0; i < KNOB_LAST; i++)
+    {
+        knob_[i].Init(seed.adc.GetMuxPtr(4, i), blockrate_);
+    }
+    for(size_t i = 0; i < CV_LAST; i++)
+    {
+        cv_[i].InitBipolarCv(seed.adc.GetPtr(i), blockrate_);
+    }
+
+    // Keyboard
+    keyboard_sr_.pin_config[DSY_SR_4021_PIN_CS]   = seed.GetPin(PIN_CD4021_CS);
+    keyboard_sr_.pin_config[DSY_SR_4021_PIN_CLK]  = seed.GetPin(PIN_CD4021_CLK);
+    keyboard_sr_.pin_config[DSY_SR_4021_PIN_DATA] = seed.GetPin(PIN_CD4021_D1);
+    keyboard_sr_.num_daisychained                 = 2;
+    keyboard_sr_.num_parallel                     = 1;
+    dsy_sr_4021_init(&keyboard_sr_);
+
+    // OLED
+    dsy_gpio_pin oled_pins[OledDisplay::NUM_PINS];
+    oled_pins[OledDisplay::DATA_COMMAND] = seed.GetPin(PIN_OLED_CMD);
+    oled_pins[OledDisplay::RESET]        = {DSY_GPIOX, 0}; // Not a real pin...
+    display_.Init(oled_pins);
+
+    // LEDs
+    // 2x PCA9685 addresses 0x00, and 0x02
+    uint8_t addr[2] = {0x00, 0x02};
+    dsy_led_driver_init(&seed.LED_DRIVER_I2C, addr, 2);
+
+    // Gate In
+    dsy_gpio_pin gate_in_pin;
+    gate_in_pin = seed.GetPin(PIN_GATE_IN);
+    gate_in_.Init(&gate_in_pin);
+    // Gate Out
+    gate_out_.mode = DSY_GPIO_MODE_OUTPUT_PP;
+    gate_out_.pull = DSY_GPIO_NOPULL;
+    gate_out_.pin  = seed.GetPin(PIN_GATE_OUT);
+    dsy_gpio_init(&gate_out_);
+}
+
+void DaisyField::VegasMode()
+{
+    uint32_t now;
+    now = dsy_system_getnow();
+    size_t idx;
+    float  key_bright, knob_bright;
+    // Cycle all 16 LEDs on keyboard SM in opposite pattern or something
+    uint8_t led_grp_a[] = {LED_KEY_A8,
+                           LED_KEY_A7,
+                           LED_KEY_A6,
+                           LED_KEY_A5,
+                           LED_KEY_A4,
+                           LED_KEY_A3,
+                           LED_KEY_A2,
+                           LED_KEY_A1};
+    uint8_t led_grp_b[] = {LED_KEY_B8,
+                           LED_KEY_B7,
+                           LED_KEY_B6,
+                           LED_KEY_B5,
+                           LED_KEY_B4,
+                           LED_KEY_B3,
+                           LED_KEY_B2,
+                           LED_KEY_B1};
+    uint8_t led_grp_c[] = {LED_KNOB_1,
+                           LED_KNOB_2,
+                           LED_KNOB_3,
+                           LED_KNOB_4,
+                           LED_KNOB_5,
+                           LED_KNOB_6,
+                           LED_KNOB_7,
+                           LED_KNOB_8};
+    if(now - last_led_update_ > 10)
+    {
+        idx        = (now >> 8) % 8;
+        key_bright = (now & 511) > 255;
+        // Knob LEDs dance in order
+        for(size_t i = 0; i < 8; i++)
+        {
+            dsy_led_driver_set_led(led_grp_a[i], cube(key_bright));
+            dsy_led_driver_set_led(led_grp_b[i], cube(1.0f - key_bright));
+            dsy_led_driver_set_led(led_grp_c[i], key_bright);
+        }
+        // OLED moves a bar across the screen
+        uint32_t bar_x = (now >> 4) % SSD1309_WIDTH;
+        display_.Fill(false);
+        for(size_t i = 0; i < SSD1309_HEIGHT; i++)
+        {
+            display_.DrawPixel(bar_x, i, true);
+        }
+        display_.Update();
+        dsy_led_driver_update();
+        dsy_led_driver_update();
+    }
+}
--- src/daisy_field.h
+++ src/daisy_field.h
@@ -3,40 +3,6 @@
 #define DSY_FIELD_BSP_H /**< & */
 #include "daisy_seed.h"
 
-#ifndef SAMPLE_RATE
-#define SAMPLE_RATE DSY_AUDIO_SAMPLE_RATE /**< & */
-#endif
-
-// Hardware related defines.
-#define SW_1_PIN 29    /**< & */
-#define SW_2_PIN 28    /**< & */
-#define SW_3_PIN 27    /**< & */
-#define GATE_OUT_PIN 0 /**< & */
-#define GATE_IN_PIN 1  /**< & */
-
-// keyboard switches shift register
-#define KB_SW_SR_CS_PIN 8  /**< & */
-#define KB_SW_SR_CLK_PIN 9 /**< & */
-#define KB_SW_SR_D1_PIN 10 /**< & */
-#define KB_SW_SR_D2_PIN 11 /**< & */
-
-// UART for MIDI via TRS jacks on Field
-#define MIDI_OUT_PIN 14 /**< & */
-#define MIDI_IN_PIN 15  /**< & */
-
-//CD4051 Select Pin controls
-#define MUX_SEL_0_PIN 21 /**< & */
-#define MUX_SEL_1_PIN 20 /**< & */
-#define MUX_SEL_2_PIN 19 /**< & */
-
-#define MUX_ADC_PIN 16 /**< & */
-#define CV1_ADC_PIN 17 /**< & */
-#define CV2_ADC_PIN 18 /**< & */
-#define CV3_ADC_PIN 23 /**< & */
-#define CV4_ADC_PIN 22 /**< & */
-
-#define LED_DRIVER_I2C i2c1_handle /**< & */
-
 /**
    @brief Hardware defines and helpers for daisy field platform.
 */
@@ -45,169 +11,189 @@ namespace daisy
 /** @addtogroup boards
     @{
 */
-
-/** enums for controls, etc.
+class DaisyField
+{
+  public:
+    /** enums for controls, etc.
 */
-enum
-{
-    SW_2,    /**< tactile switch */
-    SW_1,    /**< tactile switch */
-    SW_3,    /**< toggle */
-    SW_LAST, /**< & */
-};
-
-/** All knobs connect to ADC1_INP10 via CD4051 mux
-*/
-enum
-{
-    KNOB_1,    /**< & */
-    KNOB_3,    /**< & */
-    KNOB_5,    /**< & */
-    KNOB_2,    /**< & */
-    KNOB_4,    /**< & */
-    KNOB_6,    /**< & */
-    KNOB_7,    /**< & */
-    KNOB_8,    /**< & */
-    KNOB_LAST, /**< & */
-};
-
-enum
-{
-    CV_1,    /** Connected to ADC1_INP17 */
-    CV_2,    /** Connected  to ADC1_INP15 */
-    CV_3,    /** Connected to ADC1_INP4 */
-    CV_4,    /** Connected to ADC1_INP11 */
-    CV_LAST, /**< & */
-};
-
-enum
-{
-    LED_KEY_A8, /**< & */
-    LED_KEY_A7, /**< & */
-    LED_KEY_A6, /**< & */
-    LED_KEY_A5, /**< & */
-    LED_KEY_A4, /**< & */
-    LED_KEY_A3, /**< & */
-    LED_KEY_A2, /**< & */
-    LED_KEY_A1, /**< & */
-    LED_KEY_B1, /**< & */
-    LED_KEY_B2, /**< & */
-    LED_KEY_B3, /**< & */
-    LED_KEY_B4, /**< & */
-    LED_KEY_B5, /**< & */
-    LED_KEY_B6, /**< & */
-    LED_KEY_B7, /**< & */
-    LED_KEY_B8, /**< & */
-    LED_KNOB_1, /**< & */
-    LED_KNOB_2, /**< & */
-    LED_KNOB_3, /**< & */
-    LED_KNOB_4, /**< & */
-    LED_KNOB_5, /**< & */
-    LED_KNOB_6, /**< & */
-    LED_KNOB_7, /**< & */
-    LED_KNOB_8, /**< & */
-    LED_SW_1,   /**< & */
-    LED_SW_2,   /**< & */
-    LED_LAST    /**< & */
-};
-
-/** Struct containing hardware defines and daisy seed */
-typedef struct
-{
-    daisy::DaisySeed seed;               /**< Daisy seed */
-    daisy::Switch    switches[SW_LAST];  /**< Array of hardware switches */
-    dsy_gpio         gate_in,            /**< Gate input. */
-        gate_out;                        /**< Gate output */
-    dsy_sr_4021_handle keyboard_sr;      /**< Keyboard shift register*/
-    AnalogControl      knobs[KNOB_LAST]; /**< Array of hardware knobs*/
-    AnalogControl      cvs[CV_LAST];     /**< Array of cv ins */
-} daisy_field;
-
-/** Initializes daisy field 
-    \param p daisy_field struct to initialize
- */
-FORCE_INLINE void daisy_field_init(daisy_field *p)
-{
-    uint8_t sw_pins[SW_LAST] = {SW_1_PIN, SW_2_PIN, SW_3_PIN};
-    //    dsy_gpio_port sw_ports[SW_LAST]            = {SW_1_PORT, SW_2_PORT, SW_3_PORT};
-
-    // Init Daisy Seed
-    p->seed.Configure();
-    p->seed.Init();
-
-    // Init Switches
-    for(uint8_t i = 0; i < SW_LAST; i++)
+    enum
     {
-        p->switches[i].Init(p->seed.GetPin(sw_pins[i]), 1000.0f);
+        SW_1,    /**< tactile switch */
+        SW_2,    /**< tactile switch */
+        SW_LAST, /**< & */
+    };
+
+    /** All knobs connect to ADC1_INP10 via CD4051 mux
+*/
+    enum
+    {
+        KNOB_1,    /**< & */
+        KNOB_2,    /**< & */
+        KNOB_3,    /**< & */
+        KNOB_4,    /**< & */
+        KNOB_5,    /**< & */
+        KNOB_6,    /**< & */
+        KNOB_7,    /**< & */
+        KNOB_8,    /**< & */
+        KNOB_LAST, /**< & */
+    };
+
+    enum
+    {
+        CV_1,    /** Connected to ADC1_INP17 */
+        CV_2,    /** Connected  to ADC1_INP15 */
+        CV_3,    /** Connected to ADC1_INP4 */
+        CV_4,    /** Connected to ADC1_INP11 */
+        CV_LAST, /**< & */
+    };
+
+    enum
+    {
+        LED_KEY_A8, /**< & */
+        LED_KEY_A7, /**< & */
+        LED_KEY_A6, /**< & */
+        LED_KEY_A5, /**< & */
+        LED_KEY_A4, /**< & */
+        LED_KEY_A3, /**< & */
+        LED_KEY_A2, /**< & */
+        LED_KEY_A1, /**< & */
+        LED_KEY_B1, /**< & */
+        LED_KEY_B2, /**< & */
+        LED_KEY_B3, /**< & */
+        LED_KEY_B4, /**< & */
+        LED_KEY_B5, /**< & */
+        LED_KEY_B6, /**< & */
+        LED_KEY_B7, /**< & */
+        LED_KEY_B8, /**< & */
+        LED_KNOB_1, /**< & */
+        LED_KNOB_2, /**< & */
+        LED_KNOB_3, /**< & */
+        LED_KNOB_4, /**< & */
+        LED_KNOB_5, /**< & */
+        LED_KNOB_6, /**< & */
+        LED_KNOB_7, /**< & */
+        LED_KNOB_8, /**< & */
+        LED_SW_1,   /**< & */
+        LED_SW_2,   /**< & */
+        LED_LAST    /**< & */
+    };
+
+    DaisyField() {}
+    ~DaisyField() {}
+
+    /**Initializes the Daisy Field, and all of its hardware.*/
+    void Init();
+
+    /** Starts the Audio Engine, calling the specified interleaving callback when new data is ready */
+    void StartAudio(dsy_audio_callback cb)
+    {
+        dsy_audio_set_callback(DSY_AUDIO_INTERNAL, cb);
+        dsy_audio_start(DSY_AUDIO_INTERNAL);
     }
 
-    // Init Gate Input
-    p->gate_in.pin  = p->seed.GetPin(GATE_IN_PIN);
-    p->gate_in.mode = DSY_GPIO_MODE_INPUT;
-    p->gate_in.pull = DSY_GPIO_NOPULL;
-    dsy_gpio_init(&p->gate_in);
-
-    // Init Gate Output
-    p->gate_out.pin  = p->seed.GetPin(GATE_OUT_PIN);
-    p->gate_out.mode = DSY_GPIO_MODE_OUTPUT_PP;
-    p->gate_out.pull = DSY_GPIO_NOPULL;
-    dsy_gpio_init(&p->gate_out);
-
-    /* Init LED Driver
-       2x PCA9685 addresses 0x00, and 0x01
-       TODO: add multidriver support
-    */
-    uint8_t addr[2] = {0x00, 0x02};
-
-    dsy_led_driver_init(&p->seed.LED_DRIVER_I2C, addr, 2);
-
-    //Init Keyboard Switches
-    //TODO: add cd4021 with parallel data support
-
-    p->keyboard_sr.pin_config[DSY_SR_4021_PIN_CS]
-        = p->seed.GetPin(KB_SW_SR_CS_PIN);
-    p->keyboard_sr.pin_config[DSY_SR_4021_PIN_CLK]
-        = p->seed.GetPin(KB_SW_SR_CLK_PIN);
-    p->keyboard_sr.pin_config[DSY_SR_4021_PIN_DATA]
-        = p->seed.GetPin(KB_SW_SR_D1_PIN);
-    p->keyboard_sr.pin_config[DSY_SR_4021_PIN_DATA2]
-        = p->seed.GetPin(KB_SW_SR_D2_PIN);
-    p->keyboard_sr.num_daisychained = 1;
-    p->keyboard_sr.num_parallel     = 2;
-    dsy_sr_4021_init(&p->keyboard_sr);
-
-    // Init ADC (currently in daisy_seed).
-    AdcChannelConfig cfg[5];
-
-    // Set up mux pin
-    cfg[0].InitMux(p->seed.GetPin(MUX_ADC_PIN),
-                   8,
-                   p->seed.GetPin(MUX_SEL_0_PIN),
-                   p->seed.GetPin(MUX_SEL_1_PIN),
-                   p->seed.GetPin(MUX_SEL_2_PIN));
-
-    // Set up CV inputs
-    cfg[1].InitSingle(p->seed.GetPin(CV1_ADC_PIN));
-    cfg[2].InitSingle(p->seed.GetPin(CV2_ADC_PIN));
-    cfg[3].InitSingle(p->seed.GetPin(CV3_ADC_PIN));
-    cfg[4].InitSingle(p->seed.GetPin(CV4_ADC_PIN));
-
-    // Init all 5 channels
-    p->seed.adc.Init(cfg, 5);
-
-    // Setup Knob/CV Analog Controls Mapped to ADCs
-    for(size_t i = 0; i < KNOB_LAST; i++)
+    /** Starts the Audio Engine, calling the specified non-interleaving callback when new data is ready */
+    void StartAudio(dsy_audio_mc_callback cb)
     {
-        p->knobs[i].Init(p->seed.adc.GetMuxPtr(0, i), 1000.0f);
+        dsy_audio_set_mc_callback(cb);
+        dsy_audio_start(DSY_AUDIO_INTERNAL);
     }
-    p->cvs[CV_1].InitBipolarCv(p->seed.adc.GetPtr(1), 1000.0f);
-    p->cvs[CV_2].InitBipolarCv(p->seed.adc.GetPtr(2), 1000.0f);
-    p->cvs[CV_3].InitBipolarCv(p->seed.adc.GetPtr(3), 1000.0f);
-    p->cvs[CV_4].InitBipolarCv(p->seed.adc.GetPtr(4), 1000.0f);
 
-    // Start timer
-    dsy_tim_start();
-}
+    /** Starts Transfering data from the ADC */
+    void StartAdc() { seed.adc.Start(); }
+
+    /** Returns the samplerate of the audio engine. */
+    inline float SampleRate() const { return samplerate_; }
+
+    /** Returns the rate at which the audio callback is called. */
+    inline float BlockRate() const { return blockrate_; }
+
+    /** Returns the size of the audio buffer in frames .*/
+    inline size_t BlockSize() const { return blocksize_; }
+
+    /** Processes the ADC inputs, updating their values */
+    void ProcessAnalogControls()
+    {
+        for(size_t i = 0; i < KNOB_LAST; i++)
+            knob_[i].Process();
+        for(size_t i = 0; i < CV_LAST; i++)
+            cv_[i].Process();
+    }
+
+    /** Debounces, the tactile switches and keyboard states */
+    void UpdateDigitalControls()
+    {
+        for(size_t i = 0; i < SW_LAST; i++)
+        {
+            sw_[i].Debounce();
+        }
+        // Keyboard SM
+        dsy_sr_4021_update(&keyboard_sr_);
+        for(size_t i = 0; i < 16; i++)
+        {
+            keyboard_state_[i] = dsy_sr_4021_state(&keyboard_sr_, i)
+                                 | (keyboard_state_[i] << 1);
+        }
+    }
+
+    inline bool KeyboardState(size_t idx) const
+    {
+        return keyboard_state_[idx] == 0x00;
+    }
+
+    inline bool KeyboardRisingEdge(size_t idx) const
+    {
+        return keyboard_state_[idx] == 0x80;
+    }
+
+    inline bool KeyboardFallingEdge(size_t idx) const
+    {
+        return keyboard_state_[idx] == 0x7F;
+    }
+
+    inline float GetKnobValue(size_t idx) const
+    {
+        return knob_[idx < KNOB_LAST ? idx : 0].Value();
+    }
+
+    inline float GetCvValue(size_t idx) const
+    {
+        return cv_[idx < CV_LAST ? idx : 0].Value();
+    }
+
+    inline Switch* GetSwitch(size_t idx)
+    {
+        return &sw_[idx < SW_LAST ? idx : 0];
+    }
+
+    inline AnalogControl* GetKnob(size_t idx)
+    {
+        return &knob_[idx < KNOB_LAST ? idx : 0];
+    }
+
+    inline AnalogControl* GetCv(size_t idx)
+    {
+        return &cv_[idx < CV_LAST ? idx : 0];
+    }
+
+    /**Light show, cycling through all LEDs, and OLED
+     **/
+    void VegasMode();
+
+    DaisySeed seed;
+
+  private:
+    float              samplerate_, blockrate_;
+    size_t             blocksize_;
+    Switch             sw_[SW_LAST];
+    dsy_sr_4021_handle keyboard_sr_;
+    GateIn             gate_in_;
+    dsy_gpio           gate_out_;
+    AnalogControl      knob_[KNOB_LAST];
+    AnalogControl      cv_[CV_LAST];
+    OledDisplay        display_;
+    uint8_t            keyboard_state_[16];
+    uint32_t           last_led_update_; // for vegas mode
+};
 
 /** @} */
 
