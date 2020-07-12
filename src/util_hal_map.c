580451ba2e0f4b2933d95538c2d81ccfa9a43f8e
TheSlowGrowth <9356320+TheSlowGrowth@users.noreply.github.com>
2020-07-12 23:29:36 +0200

    Reworked I2C driver to use the DMA and a C++ interface

--- src/util_hal_map.c
+++ src/util_hal_map.c
@@ -1,5 +1,4 @@
 #include "daisy_core.h"
-#include "per_i2c.h"
 
 #include "stm32h7xx_hal.h"
 
@@ -42,13 +41,6 @@ const static uint16_t gpio_hal_pin_map[16] = {
     GPIO_PIN_15,
 };
 
-// I2C HAL MAP INSTANCES
-I2C_HandleTypeDef hi2c1;
-I2C_HandleTypeDef hi2c2;
-I2C_HandleTypeDef hi2c3;
-I2C_HandleTypeDef hi2c4;
-
-
 // GPIO FUNCTIONS
 
 GPIO_TypeDef* dsy_hal_map_get_port(const dsy_gpio_pin* p)
@@ -58,14 +50,6 @@ GPIO_TypeDef* dsy_hal_map_get_port(const dsy_gpio_pin* p)
 uint16_t dsy_hal_map_get_pin(const dsy_gpio_pin* p)
 {
     return (uint16_t)gpio_hal_pin_map[p->pin];
-}
-
-// I2C FUNCTIONS
-
-I2C_HandleTypeDef* dsy_hal_map_get_i2c(const dsy_i2c_handle* p)
-{
-    I2C_HandleTypeDef* ptr[4] = {&hi2c1, &hi2c2, &hi2c3, &hi2c4};
-    return ptr[p->periph];
 }
 
 // ADC FUNCTIONS
