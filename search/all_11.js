bdd52f6da2dcc3cbc7d04bbd84dab290ff9782a1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-27 21:04:05 +0000

    Deploy electro-smith/libDaisy to github.com/electro-smith/libDaisy.git:gh-pages

--- search/all_11.js
+++ search/all_11.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['r',['r',['../structcodec__frame__t.html#af2cb35aeb7028aed988bb028c9ec15cd',1,'codec_frame_t']]],
   ['raw_5fdata',['raw_data',['../structdaisy_1_1_wav_file_info.html#ad1e3c8f4c720cbbdfbdf26b5a2ea2903',1,'daisy::WavFileInfo']]],
+  ['rawstate',['RawState',['../classdaisy_1_1_switch.html#a77496bb98f169ba613821e2f1b7fc243',1,'daisy::Switch']]],
   ['read',['Read',['../classdaisy_1_1_ring_buffer.html#a112c8553ddc197ad491be9bd8ffabaf9',1,'daisy::RingBuffer::Read()'],['../classdaisy_1_1_ring_buffer_3_01_t_00_010_01_4.html#a5135f3de87d57d04dbde0ab633370251',1,'daisy::RingBuffer&lt; T, 0 &gt;::Read()']]],
   ['read_5fcmd',['READ_CMD',['../group__flash.html#ga21623e2a5501c821da54dd76ffc1d077',1,'READ_CMD():&#160;dev_flash_IS25LP064A.h'],['../group__flash.html#ga21623e2a5501c821da54dd76ffc1d077',1,'READ_CMD():&#160;dev_flash_IS25LP080D.h']]],
   ['read_5fext_5fread_5fparam_5fcmd',['READ_EXT_READ_PARAM_CMD',['../group__flash.html#ga3d390e71fdce7913eebf99002b1945b8',1,'dev_flash_IS25LP080D.h']]],
@@ -13,7 +14,7 @@ var searchData=
   ['read_5fserial_5fflash_5fdisco_5fparam_5fcmd',['READ_SERIAL_FLASH_DISCO_PARAM_CMD',['../group__flash.html#ga216fff040d8bab7b5df5591ebfc2c5d6',1,'READ_SERIAL_FLASH_DISCO_PARAM_CMD():&#160;dev_flash_IS25LP064A.h'],['../group__flash.html#ga216fff040d8bab7b5df5591ebfc2c5d6',1,'READ_SERIAL_FLASH_DISCO_PARAM_CMD():&#160;dev_flash_IS25LP080D.h']]],
   ['read_5fstatus_5freg_5fcmd',['READ_STATUS_REG_CMD',['../group__flash.html#gaa3c6b4a899c5b0875dfcf503f1e41b63',1,'READ_STATUS_REG_CMD():&#160;dev_flash_IS25LP064A.h'],['../group__flash.html#gaa3c6b4a899c5b0875dfcf503f1e41b63',1,'READ_STATUS_REG_CMD():&#160;dev_flash_IS25LP080D.h']]],
   ['read_5funique_5fid',['READ_UNIQUE_ID',['../group__flash.html#gab326593933b21da8d3fb2765aca1d282',1,'READ_UNIQUE_ID():&#160;dev_flash_IS25LP064A.h'],['../group__flash.html#gab326593933b21da8d3fb2765aca1d282',1,'READ_UNIQUE_ID():&#160;dev_flash_IS25LP080D.h']]],
-  ['readable',['readable',['../classdaisy_1_1_ring_buffer.html#a26d667a6e165a89ed852fdd319f90e9b',1,'daisy::RingBuffer::readable()'],['../classdaisy_1_1_ring_buffer_3_01_t_00_010_01_4.html#af1d8399be70899cf7d86875d59c70995',1,'daisy::RingBuffer&lt; T, 0 &gt;::readable()'],['../classdaisy_1_1_uart_handler.html#a4b8cad10b4f9a9520d2248c567babcc1',1,'daisy::UartHandler::Readable()']]],
+  ['readable',['Readable',['../classdaisy_1_1_uart_handler.html#a4b8cad10b4f9a9520d2248c567babcc1',1,'daisy::UartHandler::Readable()'],['../classdaisy_1_1_ring_buffer.html#a26d667a6e165a89ed852fdd319f90e9b',1,'daisy::RingBuffer::readable()'],['../classdaisy_1_1_ring_buffer_3_01_t_00_010_01_4.html#af1d8399be70899cf7d86875d59c70995',1,'daisy::RingBuffer&lt; T, 0 &gt;::readable()']]],
   ['receivecallback',['ReceiveCallback',['../class_usb_handle.html#a33ca9123252e485a3d2045590e5e6217',1,'UsbHandle::ReceiveCallback()'],['../class_usb_handle.html#a33ca9123252e485a3d2045590e5e6217',1,'UsbHandle::ReceiveCallback()']]],
   ['red',['Red',['../classdaisy_1_1_color.html#aa42ce25f0e315b50d48a08459b89a6c0',1,'daisy::Color::Red() const '],['../classdaisy_1_1_color.html#af93d31d5fc63705c228bf9da407c9a40a8b805ea394bedf4786fd371ea10283b8',1,'daisy::Color::RED()']]],
   ['relcardadd',['RelCardAdd',['../struct_d_s_y___s_d___card_info_type_def.html#a5c530fb4ff9a3032125d75a44e9b0a0a',1,'DSY_SD_CardInfoTypeDef']]],
