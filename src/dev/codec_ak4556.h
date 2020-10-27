8084d2e4552eaec007d4c3cc61cfd046fcdba7d7
stephenhensley <stephen.p.hensley@gmail.com>
2020-10-27 14:29:57 -0700

    added extern C for inclusion in cpp files, and added explicit pin reset before set strobe.

--- src/dev/codec_ak4556.h
+++ src/dev/codec_ak4556.h
@@ -2,6 +2,10 @@
 #ifndef DSY_CODEC_AK4556_H
 #define DSY_CODEC_AK4556_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "daisy_core.h"
 
 /** 
@@ -17,6 +21,10 @@
     \param reset_pin should be a dsy_gpio_pin that is connected to the RST pin of the AK4556
 */
 void codec_ak4556_init(dsy_gpio_pin reset_pin);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 /** @} */
