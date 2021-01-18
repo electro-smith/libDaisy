66de63ffd07aee00e6b0edd4eea446143034e9eb
stephenhensley <stephen.p.hensley@gmail.com>
2021-01-18 13:44:10 -0800

    trimmed old code out of files. Probably ready for merge until new features are added.

--- src/dev/codec_wm8731.h
+++ src/dev/codec_wm8731.h
@@ -113,39 +113,3 @@ class Wm8731
 
 
 #endif
-
-
-/** OLD CODE BELOW*/
-#if 0
-/** 
-    @brief Driver for the WM8731 Codec
-*/
-
-/** @addtogroup codec
-    @{
-*/
-
-/** 
-    Resets the WM8731
-    \param *hi2c array of pins handling i2c?
-    \param mcu_is_master &
-    \param sample_rate Sample rate to run codec at
-    \param bitdepth Bit depth to run codec at
-*/
-uint8_t codec_wm8731_init(dsy_i2c_handle *hi2c,
-                          uint8_t         mcu_is_master,
-                          int32_t         sample_rate,
-                          uint8_t         bitdepth);
-
-/**
-   Put codec into bypass mode
-   \param *hi2c pins handling i2c
-*/
-uint8_t codec_wm8731_enter_bypass(dsy_i2c_handle *hi2c);
-
-/**
-   Take codec out of bypass mode
-   \param *hi2c pins handling i2c
-*/
-uint8_t codec_wm8731_exit_bypass(dsy_i2c_handle *hi2c);
-#endif
