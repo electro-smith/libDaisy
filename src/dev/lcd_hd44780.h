6efb08cef75e45497315c662489910e2dcaa0108
Staffan Melin <staffan.melin@oscillator.se>
2021-02-26 00:15:07 +0100

    LCD HD44780 fixed formatting

--- src/dev/lcd_hd44780.h
+++ src/dev/lcd_hd44780.h
@@ -8,39 +8,34 @@
 
 class LcdHD44780
 {
-
-public:
-
+  public:
     LcdHD44780() {}
     ~LcdHD44780() {}
 
     struct Config
     {
-        bool cursor_on;
-        bool cursor_blink;
+        bool         cursor_on;
+        bool         cursor_blink;
         dsy_gpio_pin rs, en, d4, d5, d6, d7;
     };
 
-	void Init(const Config&);
-	void Print(const char *);
-	void PrintInt(int);
-	void SetCursor(uint8_t, uint8_t);
-	void Clear();
+    void Init(const Config &);
+    void Print(const char *);
+    void PrintInt(int);
+    void SetCursor(uint8_t, uint8_t);
+    void Clear();
 
-private:
+  private:
+    bool     cursor_on;
+    bool     cursor_blink;
+    dsy_gpio lcd_pin_rs;
+    dsy_gpio lcd_pin_en;
+    dsy_gpio lcd_data_pin[4]; // D4-D7
 
-	bool cursor_on;
-	bool cursor_blink;
-	dsy_gpio lcd_pin_rs;
-	dsy_gpio lcd_pin_en;
-	dsy_gpio lcd_data_pin[4]; // D4-D7
-
-	void WriteData(uint8_t);
-	void WriteCommand(uint8_t);
-	void Write(uint8_t, uint8_t);
-
+    void WriteData(uint8_t);
+    void WriteCommand(uint8_t);
+    void Write(uint8_t, uint8_t);
 };
-
 
 
 #endif
