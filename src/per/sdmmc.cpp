04874adc4b1dbd5e7224484b8b08169a0c287744
stephenhensley <stephen.p.hensley@gmail.com>
2021-03-03 15:24:56 -0800

    made sure gpio are only initialized for used pins.

--- src/per/sdmmc.cpp
+++ src/per/sdmmc.cpp
@@ -47,27 +47,27 @@ void HAL_SD_MspInit(SD_HandleTypeDef* sdHandle)
         __HAL_RCC_GPIOC_CLK_ENABLE();
         __HAL_RCC_GPIOD_CLK_ENABLE();
         /**SDMMC1 GPIO Configuration    
-    PC12     ------> SDMMC1_CK
-    PC11     ------> SDMMC1_D3
-    PC10     ------> SDMMC1_D2
-    PD2     ------> SDMMC1_CMD
-    PC9     ------> SDMMC1_D1
-    PC8     ------> SDMMC1_D0 
-    */
-        GPIO_InitStruct.Pin
-            = GPIO_PIN_12 | GPIO_PIN_11 | GPIO_PIN_10 | GPIO_PIN_9 | GPIO_PIN_8;
-        GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull  = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        //GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+            PC12     ------> SDMMC1_CK
+            PC11     ------> SDMMC1_D3
+            PC10     ------> SDMMC1_D2
+            PD2     ------> SDMMC1_CMD
+            PC9     ------> SDMMC1_D1
+            PC8     ------> SDMMC1_D0 
+        */
+
+        GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_8;
+        if(sdHandle->Init.BusWide == SDMMC_BUS_WIDE_4B)
+            GPIO_InitStruct.Pin |= GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11;
+        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF12_SDIO1;
         HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-        GPIO_InitStruct.Pin   = GPIO_PIN_2;
-        GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull  = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        //GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+        GPIO_InitStruct.Pin       = GPIO_PIN_2;
+        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF12_SDIO1;
         HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
