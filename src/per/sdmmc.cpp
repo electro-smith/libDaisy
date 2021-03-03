274f1ee2991b3b13135888f4ec3ff4ecb5c80449
stephenhensley <stephen.p.hensley@gmail.com>
2021-03-03 15:05:22 -0800

    added configuration to SdmmcHandler and missing interrupt handler.

--- src/per/sdmmc.cpp
+++ src/per/sdmmc.cpp
@@ -1,38 +1,33 @@
 #include "per/sdmmc.h"
 #include "util/hal_map.h"
-//#include "util/bsp_sd_diskio.h"
-// Actual FatFS
-//#include "ff.h"
-//#include "ff_gen_drv.h"
-
-#include "fatfs.h"
+//#include "fatfs.h"
 
 
 using namespace daisy;
 
+/** Local HAL handle */
 SD_HandleTypeDef hsd1;
-// Fat stuff
-//uint8_t retSD;
-//char    SDPath[4];
-//FATFS   SDFatFS;
-//FIL     SDFile;
-//// Driver Link
-// TODO: Probably separate this into drivers for
-// SD diskIO and FATFS instead of lumping it all together.
-// Especially since FATFS can be set up on the SDRAM, QSPI,
-//  USB, and SD Card.
 
-void SdmmcHandler::Init()
+SdmmcHandler::Result SdmmcHandler::Init(const Config& cfg)
 {
-    hsd1.Instance                 = SDMMC1;
-    hsd1.Init.ClockEdge           = SDMMC_CLOCK_EDGE_RISING;
-    hsd1.Init.ClockPowerSave      = SDMMC_CLOCK_POWER_SAVE_DISABLE;
-    hsd1.Init.BusWide             = SDMMC_BUS_WIDE_4B;
+    hsd1.Instance            = SDMMC1;
+    hsd1.Init.ClockEdge      = SDMMC_CLOCK_EDGE_RISING;
+    hsd1.Init.ClockPowerSave = cfg.clock_powersave
+                                   ? SDMMC_CLOCK_POWER_SAVE_ENABLE
+                                   : SDMMC_CLOCK_POWER_SAVE_DISABLE;
+    hsd1.Init.BusWide = cfg.width == BusWidth::BITS_1 ? SDMMC_BUS_WIDE_1B
+                                                            : SDMMC_BUS_WIDE_4B;
     hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
-    //  hsd1.Init.ClockDiv = 168; // 476.2kHz works
-    hsd1.Init.ClockDiv = 6; // 12MHz works
-                            // FatFs stuff.
-    //  dsy_fatfs_init();
+
+    switch(cfg.speed)
+    {
+        case Speed::SLOW: hsd1.Init.ClockDiv = 250; break;
+        case Speed::MEDIUM_SLOW: hsd1.Init.ClockDiv = 8; break;
+        case Speed::STANDARD: hsd1.Init.ClockDiv = 4; break;
+        case Speed::FAST: hsd1.Init.ClockDiv = 2; break;
+        case Speed::VERY_FAST: hsd1.Init.ClockDiv = 1; break;
+    }
+    return Result::OK;
 }
 
 
@@ -61,16 +56,18 @@ void HAL_SD_MspInit(SD_HandleTypeDef* sdHandle)
     */
         GPIO_InitStruct.Pin
             = GPIO_PIN_12 | GPIO_PIN_11 | GPIO_PIN_10 | GPIO_PIN_9 | GPIO_PIN_8;
-        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull  = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        //GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF12_SDIO1;
         HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-        GPIO_InitStruct.Pin       = GPIO_PIN_2;
-        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+        GPIO_InitStruct.Pin   = GPIO_PIN_2;
+        GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull  = GPIO_NOPULL;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+        //GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF12_SDIO1;
         HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
@@ -113,4 +110,9 @@ void HAL_SD_MspDeInit(SD_HandleTypeDef* sdHandle)
 
         /* USER CODE END SDMMC1_MspDeInit 1 */
     }
+}
+
+extern "C"
+{
+    void SDMMC1_IRQHandler() { HAL_SD_IRQHandler(&hsd1); }
 }
