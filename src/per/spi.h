7a5ea84dbe0f64ac5dc9d93c40ad68bebaa9ec75
Ben Sergentanis <bensergentanis@gmail.com>
2021-05-26 15:07:55 -0400

    Spi update (#347)
    * Add simple config struct
    
    * Add simpl pimpl
    
    * Add basic set of config options
    
    * Add config to oled and correct Mode names
    
    * Fix style
    
    * Add error handler to BlockingTransmit
    
    * Add MapInstanceToHandle
    
    TODO pin config, clock init deinit, etc
    
    * Create pin config and pincheck arrays
    
    * Add functions to init and deinit pins
    
    * Add pin config to oled spi
    
    * Remove extra braces from oled
    
    * Skip init mosi/miso under certain direct./mode
    
    * Pull ss enable out to variable
    
    * Fix compilation errors
    
    The global pin config things were conflicting with the ones from uart
    temp fix is to name them all foo_spi, but this is a bad long term fix
    they should probably be members of impl
    
    also, the include for hal_map had to be in a extern C thingy
    
    * Create spi PollReceive function
    
    * Add missing wrapper for pollreceive
    
    * Add GetConfig and GetError to spi
    
    * Remove add config from TODO
    
    * Remove unused enum SpiPin
    
    * Change BAUDRATEPRESCALER_N to PS_N
    
    * Change PollReceive to BlockingReceive
    
    * Add timeout argument to blocking transmit
    
    defaults to 100 ms
    
    * Mark all pin_alt_spi arrays static

--- src/per/spi.h
+++ src/per/spi.h
@@ -6,7 +6,6 @@
 
 /* TODO:
 - Add documentation
-- Add configuration
 - Add reception
 - Add IT
 - Add DMA
@@ -18,40 +17,123 @@ namespace daisy
 @{
 */
 
-/** SPI peripheral enum */
-enum SpiPeriph
-{
-    SPI_PERIPH_1, /**< SPI peripheral 1 */
-    SPI_PERIPH_3, /**< SPI peripheral 3 */
-    SPI_PERIPH_6, /**< SPI peripheral 3 */
-};
-
-/** SPI pins */
-enum SpiPin
-{
-    SPI_PIN_CS,   /**< CS pin */
-    SPI_PIN_SCK,  /**< SCK pin */
-    SPI_PIN_MOSI, /**< MOSI pin */
-    SPI_PIN_MISO, /**< MISO pin */
-};
-
 /**  Handler for serial peripheral interface */
 class SpiHandle
 {
   public:
-    SpiHandle() {}
-    ~SpiHandle() {}
+    struct Config
+    {
+        enum class Peripheral
+        {
+            SPI_1,
+            SPI_2,
+            SPI_3,
+            SPI_4,
+            SPI_5,
+            SPI_6,
+        };
+
+        enum class Mode
+        {
+            MASTER,
+            SLAVE,
+        };
+
+        enum class Direction
+        {
+            TWO_LINES,
+            TWO_LINES_TX_ONLY,
+            TWO_LINES_RX_ONLY,
+            ONE_LINE,
+        };
+
+        enum class ClockPolarity
+        {
+            LOW,
+            HIGH,
+        };
+
+        enum class ClockPhase
+        {
+            ONE_EDGE,
+            TWO_EDGE,
+        };
+
+        enum class NSS
+        {
+            SOFT,
+            HARD_INPUT,
+            HARD_OUTPUT,
+        };
+
+        enum class BaudPrescaler
+        {
+            PS_2,
+            PS_4,
+            PS_8,
+            PS_16,
+            PS_32,
+            PS_64,
+            PS_128,
+            PS_256,
+        };
+
+        struct
+        {
+            dsy_gpio_pin sclk; /**< & */
+            dsy_gpio_pin miso; /**< & */
+            dsy_gpio_pin mosi; /**< & */
+            dsy_gpio_pin nss;  /**< & */
+        } pin_config;
+
+        Peripheral    periph;
+        Mode          mode;
+        Direction     direction;
+        unsigned long datasize;
+        ClockPolarity clock_polarity;
+        ClockPhase    clock_phase;
+        NSS           nss;
+        BaudPrescaler baud_prescaler;
+    };
+
+    SpiHandle() : pimpl_(nullptr) {}
+    SpiHandle(const SpiHandle& other) = default;
+    SpiHandle& operator=(const SpiHandle& other) = default;
+
+    /** Return values for Spi functions. */
+    enum class Result
+    {
+        OK, /**< & */
+        ERR /**< & */
+    };
 
     /** Initializes handler */
-    void Init();
+    Result Init(const Config& config);
+
+    /** Returns the current config. */
+    const Config& GetConfig() const;
 
     /** Blocking transmit 
     \param *buff input buffer
     \param size  buffer size
     */
-    void BlockingTransmit(uint8_t *buff, size_t size);
+    Result BlockingTransmit(uint8_t* buff, size_t size, uint32_t timeout = 100);
+
+    /** Polling Receive
+    \param *buff input buffer
+    \param size  buffer size
+    \param timeout How long to timeout for
+    \return Whether the receive was successful or not
+    */
+    Result BlockingReceive(uint8_t* buffer, uint16_t size, uint32_t timeout);
+
+    /** \return the result of HAL_SPI_GetError() to the user. */
+    int CheckError();
+
+    class Impl; /**< SPI implementation */
 
   private:
+    Impl* pimpl_;
 };
 
 
