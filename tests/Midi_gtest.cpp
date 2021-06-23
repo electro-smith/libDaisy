12f6c29d789aa6d7db95105689b6816699ef51d8
beserge <bensergentanis@gmail.com>
2021-06-23 11:46:52 -0400

    Simple midi test sanity check

--- tests/Midi_gtest.cpp
+++ tests/Midi_gtest.cpp
@@ -1,5 +1,9 @@
-#include "hid/midi.h"
 #include <gtest/gtest.h>
+#include "hid/midi.h"
+
+//get rid of compiler errors over unused args in stubs
+#define UNUSED(x) (void)x
+#define MAX_MESSAGES 3000
 
 using namespace daisy;
 
@@ -9,13 +13,34 @@ class MidiTestTransport{
     ~MidiTestTransport() {}
 
     struct Config {};
-    void Init(Config conf) {};
-}
+
+    void Init(Config conf) { UNUSED(conf); }
+
+    //stubs to make the handler happy
+    void StartRx() {};
+    size_t Readable() { return 1; }
+    void FlushRx() { }
+    void Tx(uint8_t* buff, size_t size) { UNUSED(buff); UNUSED(size); }
+    uint8_t Rx() { return 1; }    
+    bool RxActive() { return true; }
+
+    private:
+};
 
 TEST(util_MidiHandler, a_stateAfterInit){
     MidiHandler<MidiTestTransport> midi;
-    MidiHandler<MidiTestTransport>::Config conf;
+    MidiHandler<MidiTestTransport>::Config handler_conf;
+    midi.Init(handler_conf);
     
-    midi.Init(conf);
-    EXPECT_TRUE(true);
-}
+    midi.Parse(0x80);
+    midi.Parse(0x00);
+    midi.Parse(0x00);
+
+    MidiEvent event = midi.PopEvent();
+    EXPECT_TRUE(event.type == NoteOff);
+
+    NoteOffEvent no_event = event.AsNoteOff();
+    EXPECT_EQ(no_event.channel, 0);
+    EXPECT_EQ(no_event.note, 0);
+    EXPECT_EQ(no_event.velocity, 0);
+}    
