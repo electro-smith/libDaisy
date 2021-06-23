2a8c9be7069e6223439b2fd78e097236d90c8a51
beserge <bensergentanis@gmail.com>
2021-06-23 10:43:03 -0400

    Create midi test file

---
+++ tests/Midi_gtest.cpp
@@ -0,0 +1,21 @@
+#include "hid/midi.h"
+#include <gtest/gtest.h>
+
+using namespace daisy;
+
+class MidiTestTransport{
+    public:
+    MidiTestTransport() {}
+    ~MidiTestTransport() {}
+
+    struct Config {};
+    void Init(Config conf) {};
+}
+
+TEST(util_MidiHandler, a_stateAfterInit){
+    MidiHandler<MidiTestTransport> midi;
+    MidiHandler<MidiTestTransport>::Config conf;
+    
+    midi.Init(conf);
+
+}
