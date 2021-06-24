ae530c6d8d6810402f27c67c90f0509232e01afb
beserge <bensergentanis@gmail.com>
2021-06-24 10:24:49 -0400

    Style

--- tests/Midi_gtest.cpp
+++ tests/Midi_gtest.cpp
@@ -8,68 +8,85 @@
 
 using namespace daisy;
 
-class MidiTestTransport{
-    public:
+class MidiTestTransport
+{
+  public:
     MidiTestTransport() {}
     ~MidiTestTransport() {}
 
-    struct Config {};
+    struct Config
+    {
+    };
 
     void Init(Config conf) { UNUSED(conf); }
 
     //stubs to make the handler happy
-    void StartRx() {};
+    void   StartRx(){};
     size_t Readable() { return 1; }
-    void FlushRx() { }
-    void Tx(uint8_t* buff, size_t size) { UNUSED(buff); UNUSED(size); }
-    uint8_t Rx() { return 1; }    
-    bool RxActive() { return true; }
+    void   FlushRx() {}
+    void   Tx(uint8_t* buff, size_t size)
+    {
+        UNUSED(buff);
+        UNUSED(size);
+    }
+    uint8_t Rx() { return 1; }
+    bool    RxActive() { return true; }
 
-    private:
+  private:
 };
 
-class MidiTest : public ::testing::Test {
-    protected:
-        void SetUp() {
-            MidiHandler<MidiTestTransport>::Config conf;
-            midi.Init(conf);
-        }
+class MidiTest : public ::testing::Test
+{
+  protected:
+    void SetUp()
+    {
+        MidiHandler<MidiTestTransport>::Config conf;
+        midi.Init(conf);
+    }
 
-        MidiHandler<MidiTestTransport> midi;
+    MidiHandler<MidiTestTransport> midi;
 };
 
 //Channel Voice messages
 //for now I'm not using asNoteOff, etc. easier to just test on the raw MidiEvent
-TEST_F(MidiTest, channelVoice){    
-
-    for(uint8_t type = 0; type < 7; type++){
-        for (uint8_t chn = 0; chn < 16; chn++){
-            for (uint8_t d0 = 0; d0 < 128; d0++){
-                for (uint8_t d1 = 0; d1 < 128; d1++){
+TEST_F(MidiTest, channelVoice)
+{
+    for(uint8_t type = 0; type < 7; type++)
+    {
+        for(uint8_t chn = 0; chn < 16; chn++)
+        {
+            for(uint8_t d0 = 0; d0 < 128; d0++)
+            {
+                for(uint8_t d1 = 0; d1 < 128; d1++)
+                {
                     midi.Parse(0x80 + (type << 4) + chn);
                     midi.Parse(d0);
 
-                    if(type != 4 && type != 5){ //4 and 5 are just one data byte
+                    if(type != 4 && type != 5)
+                    { //4 and 5 are just one data byte
                         midi.Parse(d1);
                     }
 
                     MidiEvent event = midi.PopEvent();
-                    
-                    if(type == 3 && d0 > 119){
+
+                    if(type == 3 && d0 > 119)
+                    {
                         EXPECT_EQ((uint8_t)event.type, (uint8_t)ChannelMode);
                     }
-                    else{
+                    else
+                    {
                         EXPECT_EQ((uint8_t)event.type, type);
                     }
 
                     EXPECT_EQ(event.channel, chn);
                     EXPECT_EQ(event.data[0], d0);
 
-                    if(type != 4 && type != 5){ //4 and 5 are just one data byte
+                    if(type != 4 && type != 5)
+                    { //4 and 5 are just one data byte
                         EXPECT_EQ(event.data[1], d1);
-                    }                                      
+                    }
                 }
             }
         }
-    }    
+    }
 }
