9d520849fa6deca604e351b852ef0344da2170e1
stephenhensley <stephen.p.hensley@gmail.com>
2021-07-21 16:56:56 -0700

    updated version in CMakeLists and version.h for new release.

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 #ifndef DSY_VERSION_H
 #define DSY_VERSION_H
 
-#define LIBDAISY_VER_MAJ 0
-#define LIBDAISY_VER_MIN 1
+#define LIBDAISY_VER_MAJ 1
+#define LIBDAISY_VER_MIN 0
 #define LIBDAISY_VER_PATCH 0
 
 #endif
