8171d1a7d7e8543a444f0ba25012e3ca2dde1035
stephenhensley <stephen.p.hensley@gmail.com>
2021-09-14 17:43:30 -0700

    started adding tests for PersistentStorage.

---
+++ tests/PersistentStorage_gtest.cpp
@@ -0,0 +1,80 @@
+#include "util/PersistentStorage.h"
+#include <gtest/gtest.h>
+
+using namespace daisy;
+
+struct StorageTestData
+{
+    StorageTestData() : a(0xdeadbeef) {}
+
+    uint32_t a;
+
+    bool operator==(const StorageTestData &rhs) { return a == rhs.a; }
+    bool operator!=(const StorageTestData &rhs) { return !operator==(rhs); }
+};
+
+using StorageTestClass = PersistentStorage<StorageTestData>;
+
+TEST(util_PersistentStorage, a_stateAfterInitClean)
+{
+    QSPIHandle       qspi;
+    StorageTestClass storage(qspi);
+    StorageTestData  defaults;
+
+    // Set default data and initialize
+    // defaults from obj. constructor
+    storage.Init(defaults);
+
+    auto state = storage.GetState();
+    auto val   = storage.GetSettings().a;
+    EXPECT_EQ(state, StorageTestClass::State::FACTORY);
+    EXPECT_EQ(val, static_cast<uint32_t>(0xdeadbeef));
+}
+
+TEST(util_PersistentStorage, a_stateAfterWrite)
+{
+    QSPIHandle       qspi;
+    StorageTestClass storage(qspi);
+    StorageTestData  defaults;
+
+    // Set default data and initialize
+    // defaults from obj. constructor
+    storage.Init(defaults);
+
+    //Write a new value and save
+    auto &data = storage.GetSettings();
+    data.a     = 0;
+    storage.Save();
+
+    auto state = storage.GetState();
+    auto val   = storage.GetSettings().a;
+    EXPECT_EQ(state, StorageTestClass::State::USER);
+    EXPECT_EQ(val, (uint32_t)0);
+}
+
+TEST(util_PersistentStorage, c_recallData)
+{
+    QSPIHandle       qspi;
+    StorageTestClass storage(qspi);
+    StorageTestData  defaults;
+    // Initialize defaults (sets to FACTORY)
+    storage.Init(defaults);
+
+    //Write a new value and save (sets to USER)
+    auto &data = storage.GetSettings();
+    data.a     = 0;
+    storage.Save();
+
+    // Need to check if the reinit can cause potential issues here,
+    // Kind of hard to write the private SaveStruct stuff to the memory
+    // pool with the private QSPIState
+
+    // Using a new instance to prevent any volatile bits from sticking through.
+    // it should still grab the expected memory pool
+    StorageTestClass newStorage(qspi);
+
+    auto  savedState = newStorage.GetState();
+    uint32_t val   = newStorage.GetSettings().a;
+    EXPECT_EQ(savedState, StorageTestClass::State::USER);
+    EXPECT_EQ(val, (uint32_t)0);
+}
