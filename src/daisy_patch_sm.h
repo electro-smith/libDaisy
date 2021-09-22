d24d4432d052b7f0a606208abc487ca3ea969484
CorvusPrudens <corvusprudens@gmail.com>
2021-09-22 11:06:39 -0600

    Revert "Updated patch sm sdram init to cpp"
    This reverts commit 0d4429108ac9512832599fc56103c31d6d3905c4.

--- src/daisy_patch_sm.h
+++ src/daisy_patch_sm.h
@@ -180,7 +180,7 @@ class DaisyPatchSM
 
     /** Direct Access Structs/Classes */
     System           system;
-    SdramHandle      sdram;
+    dsy_sdram_handle sdram;
     QSPIHandle       qspi;
     AudioHandle      audio;
     AdcHandle        adc;
