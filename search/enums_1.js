c60dd966008f58d6552b69dbd46b692f7cb36767
stephenhensley <stephenhensley@users.noreply.github.com>
2021-11-12 20:35:20 +0000

    deploy: ceb8c02fb06b279c333e28cda731dd7f35bb0684

--- search/enums_1.js
+++ search/enums_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['bitdepth_0',['BitDepth',['../classdaisy_1_1_dac_handle.html#a0c969b1aa7f8549756e6309b3922e902',1,'daisy::DacHandle::BitDepth()'],['../structdaisy_1_1_sai_handle_1_1_config.html#aafe336101d7c50dd68e2611ccce7c808',1,'daisy::SaiHandle::Config::BitDepth()']]],
-  ['bufferstate_1',['BufferState',['../classdaisy_1_1_dac_handle.html#a30248a4615012854c7271b510ba7803c',1,'daisy::DacHandle::BufferState()'],['../classdaisy_1_1_wav_writer.html#a17e4799ade3a78bfb8cab25ac90f04eb',1,'daisy::WavWriter::BufferState()']]],
-  ['buswidth_2',['BusWidth',['../classdaisy_1_1_sdmmc_handler.html#a146e7b69e3d0c1a0c0fae316040d799b',1,'daisy::SdmmcHandler']]]
+  ['boardversion_1',['BoardVersion',['../classdaisy_1_1_daisy_seed.html#a59363a85e54eebb334d8663a215e5f12',1,'daisy::DaisySeed']]],
+  ['bufferstate_2',['BufferState',['../classdaisy_1_1_dac_handle.html#a30248a4615012854c7271b510ba7803c',1,'daisy::DacHandle::BufferState()'],['../classdaisy_1_1_wav_writer.html#a17e4799ade3a78bfb8cab25ac90f04eb',1,'daisy::WavWriter::BufferState()']]],
+  ['buswidth_3',['BusWidth',['../classdaisy_1_1_sdmmc_handler.html#a146e7b69e3d0c1a0c0fae316040d799b',1,'daisy::SdmmcHandler']]]
 ];
