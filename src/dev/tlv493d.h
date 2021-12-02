f5a50ea27c7463db21346b25c03321609c5a01d4
beserge <bensergentanis@gmail.com>
2021-12-02 13:59:36 -0500

    Finish init, add IO stuff

--- src/dev/tlv493d.h
+++ src/dev/tlv493d.h
@@ -4,6 +4,13 @@
 
 #define TLV493D_ADDRESS1 0x5E
 #define TLV493D_ADDRESS2 0x1F
+#define TLV493D_BUSIF_READSIZE 10
+#define TLV493D_BUSIF_WRITESIZE 4
+#define TLV493D_NUM_OF_REGMASKS 25
+#define TLV493D_DEFAULTMODE POWERDOWNMODE
+
+#define REGMASK_READ 0
+#define REGMASK_WRITE 1
 
 namespace daisy
 {
@@ -48,12 +55,12 @@ class Tlv493dI2CTransport
         i2c_.Init(i2c_config);
     }
 
-    void Write(uint8_t* data, uint16_t size)
+    void Write(uint8_t *data, uint16_t size)
     {
         i2c_.TransmitBlocking(TLV493D_ADDRESS1, data, size, 10);
     }
 
-    void Read(uint8_t* data, uint16_t size)
+    void Read(uint8_t *data, uint16_t size)
     {
         i2c_.ReceiveBlocking(TLV493D_ADDRESS1, data, size, 10);
     }
@@ -73,6 +80,80 @@ class Tlv493d
   public:
     Tlv493d() {}
     ~Tlv493d() {}
+
+    enum Registers_e
+    {
+        R_BX1 = 0,
+        R_BX2,
+        R_BY1,
+        R_BY2,
+        R_BZ1,
+        R_BZ2,
+        R_TEMP1,
+        R_TEMP2,
+        R_FRAMECOUNTER,
+        R_CHANNEL,
+        R_POWERDOWNFLAG,
+        R_RES1,
+        R_RES2,
+        R_RES3,
+        W_PARITY,
+        W_ADDR,
+        W_INT,
+        W_FAST,
+        W_LOWPOWER,
+        W_TEMP_NEN,
+        W_LP_PERIOD,
+        W_PARITY_EN,
+        W_RES1,
+        W_RES2,
+        W_RES3
+    };
+
+    struct RegMask_t
+    {
+        uint8_t rw;
+        uint8_t byteAdress;
+        uint8_t bitMask;
+        uint8_t shift;
+    };
+
+	enum AccessMode_e
+	{
+		POWERDOWNMODE = 0,
+		FASTMODE,
+		LOWPOWERMODE,
+		ULTRALOWPOWERMODE,
+		MASTERCONTROLLEDMODE,
+	};
+
+    const RegMask_t RegMasks[] = {
+        {REGMASK_READ, 0, 0xFF, 0},  // R_BX1
+        {REGMASK_READ, 4, 0xF0, 4},  // R_BX2
+        {REGMASK_READ, 1, 0xFF, 0},  // R_BY1
+        {REGMASK_READ, 4, 0x0F, 0},  // R_BY2
+        {REGMASK_READ, 2, 0xFF, 0},  // R_BZ1
+        {REGMASK_READ, 5, 0x0F, 0},  // R_BZ2
+        {REGMASK_READ, 3, 0xF0, 4},  // R_TEMP1
+        {REGMASK_READ, 6, 0xFF, 0},  // R_TEMP2
+        {REGMASK_READ, 3, 0x0C, 2},  // R_FRAMECOUNTER
+        {REGMASK_READ, 3, 0x03, 0},  // R_CHANNEL
+        {REGMASK_READ, 5, 0x10, 4},  // R_POWERDOWNFLAG
+        {REGMASK_READ, 7, 0x18, 3},  // R_RES1
+        {REGMASK_READ, 8, 0xFF, 0},  // R_RES2
+        {REGMASK_READ, 9, 0x1F, 0},  // R_RES3
+        {REGMASK_WRITE, 1, 0x80, 7}, // W_PARITY
+        {REGMASK_WRITE, 1, 0x60, 5}, // W_ADDR
+        {REGMASK_WRITE, 1, 0x04, 2}, // W_INT
+        {REGMASK_WRITE, 1, 0x02, 1}, // W_FAST
+        {REGMASK_WRITE, 1, 0x01, 0}, // W_LOWPOWER
+        {REGMASK_WRITE, 3, 0x80, 7}, // W_TEMP_EN
+        {REGMASK_WRITE, 3, 0x40, 6}, // W_LOWPOWER
+        {REGMASK_WRITE, 3, 0x20, 5}, // W_POWERDOWN
+        {REGMASK_WRITE, 1, 0x18, 3}, // W_RES1
+        {REGMASK_WRITE, 2, 0xFF, 0}, // W_RES2
+        {REGMASK_WRITE, 3, 0x1F, 0}  // W_RES3
+    };
 
     struct Config
     {
@@ -95,23 +176,51 @@ class Tlv493d
         ResetSensor(config_.address);
 
         // get all register data from sensor
-        // tlv493d::readOut(&mInterface);
+        ReadOut();
         // copy factory settings to write registers
-        // setRegBits(tlv493d::W_RES1, getRegBits(tlv493d::R_RES1));
-        // setRegBits(tlv493d::W_RES2, getRegBits(tlv493d::R_RES2));
-        // setRegBits(tlv493d::W_RES3, getRegBits(tlv493d::R_RES3));
+        SetRegBits(W_RES1, GetRegBits(R_RES1));
+        SetRegBits(W_RES2, GetRegBits(R_RES2));
+        SetRegBits(W_RES3, GetRegBits(R_RES3));
         // enable parity detection
-        // setRegBits(tlv493d::W_PARITY_EN, 1);
+        SetRegBits(W_PARITY_EN, 1);
         // config sensor to lowpower mode
         // also contains parity calculation and writeout to sensor
-        // setAccessMode(TLV493D_DEFAULTMODE);
+        SetAccessMode(TLV493D_DEFAULTMODE);
     }
 
+    void ReadOut() { transport_.Read(regReadData, TLV493D_BUSIF_READSIZE); }
+
+    void WriteOut() { transport_.Write(regWriteData, TLV493D_BUSIF_WRITESIZE); }
+
+    void SetRegBits(uint8_t regMaskIndex, uint8_t data)
+    {
+        if(regMaskIndex < TLV493D_NUM_OF_REGMASKS)
+        {
+            SetToRegs(RegMasks[regMaskIndex], regWriteData, data);
+        }
+    }
+
+    void GetRegBits(uint8_t regMaskIndex)
+    {
+        if(regMaskIndex < TLV493D_NUM_OF_REGMASKS)
+        {
+            const RegMask_t *mask = &(RegMasks[regMaskIndex]);
+            if(mask->rw == REGMASK_READ)
+            {
+                return GetFromRegs(mask, regReadData);
+            }
+            else
+            {
+                return GetFromRegs(mask, regWriteData);
+            }
+        }
+    }
 
   private:
     Config    config_;
     Transport transport_;
-
+    uint8_t   regReadData[TLV493D_BUSIF_READSIZE];
+    uint8_t   regWriteData[TLV493D_BUSIF_WRITESIZE];
 
     // internal function called by begin()
     void ResetSensor(uint8_t adr)
@@ -130,6 +239,23 @@ class Tlv493d
         }
 
         transport_.Write(data, 2);
+    }
+
+    uint8_t GetFromRegs(const RegMask_t *mask, uint8_t *regData)
+    {
+        return (regData[mask->byteAdress] & mask->bitMask) >> mask->shift;
+    }
+
+
+    void SetToRegs(const RegMask_t *mask, uint8_t *regData, uint8_t toWrite)
+    {
+        if(mask->rw == REGMASK_WRITE)
+        {
+            uint8_t regValue = regData[mask->byteAdress];
+            regValue &= ~(mask->bitMask);
+            regValue |= (toWrite << mask->shift) & mask->bitMask;
+            regData[mask->byteAdress] = regValue;
+        }
     }
 };
 
