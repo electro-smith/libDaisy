c73e43739bdf6bc2301a69198a25d6210fdf1b75
Stephen Hensley <shensley.Audio@gmail.com>
2021-12-07 14:54:16 -0800

    C++ GPIO (#451)
    * in progress work on new gpio class (leaving old gpio in place for now
    
    * removed old C GPIO file, and finished adding base level documentation to new header.
    
    * added consts for new GPIO pins to start doing tests -- also deprecated Configure for easier portability with other board files.
    
    * Pin struct is now constexpr-friendly, and config enums have been moved outside of Config struct for simplified syntax
    
    * pinout consts have been moved to header, made constexpr, and placed in their own daisy::seed:: namespace for simplified usage syntax.
    
    * moved Pin struct to daisy_core, and added notes to dsy_gpio_pin structs mentioning their future removal
    
    * style fixes.
    
    * changed GPIO to cpp in CMakeLists.txt
    
    * added Pin and GPIOPort to daisy namespace
    
    * added unit tests for new Pin type.
    
    * style fix.
    
    * changed GPIOPort names from Pn to PORTn
    
    Co-authored-by: stephenhensley <stephen.p.hensley@gmail.com>

--- src/per/gpio.h
+++ src/per/gpio.h
@@ -2,6 +2,130 @@
 #ifndef DSY_GPIO_H
 #define DSY_GPIO_H
 #include "daisy_core.h"
+
+/** New C++ GPIO */
+#ifdef __cplusplus
+
+namespace daisy
+{
+/** @brief General Purpose I/O control */
+class GPIO
+{
+  public:
+    /** @brief Mode of operation for the specified GPIO */
+    enum class Mode
+    {
+        INPUT,     /**< Input for reading state of pin */
+        OUTPUT,    /**< Output w/ push-pull configuration */
+        OUTPUT_OD, /**< Output w/ open-drain configuration */
+        ANALOG,    /**< Analog for connection to ADC or DAC peripheral */
+    };
+
+    /** @brief Configures whether an internal Pull up or Pull down resistor is used. 
+     *  
+     * Internal Pull up/down resistors are typically 40k ohms, and will be between
+     * 30k and 50k
+     * 
+     * When the Pin is configured in Analog mode, the pull up/down resistors are
+     * disabled by hardware. 
+     */
+    enum class Pull
+    {
+        NOPULL,   /**< No pull up resistor */
+        PULLUP,   /**< Internal pull up enabled */
+        PULLDOWN, /**< Internal pull down enabled*/
+    };
+
+    /** @brief Output speed controls the drive strength, and slew rate of the pin */
+    enum class Speed
+    {
+        LOW,
+        MEDIUM,
+        HIGH,
+        VERY_HIGH,
+    };
+
+    /** @brief Configuration for a given GPIO */
+    struct Config
+    {
+        Pin   pin;
+        Mode  mode;
+        Pull  pull;
+        Speed speed;
+
+        /** Constructor with no arguments will prepare an invalid GPIO set as
+         *  an input, with no pullup. 
+         */
+        Config()
+        : pin(), mode(Mode::INPUT), pull(Pull::NOPULL), speed(Speed::LOW)
+        {
+        }
+    };
+
+    GPIO() {}
+
+    /** @brief Initialize the GPIO from a Configuration struct */
+    void Init(const Config &cfg);
+
+    /** @brief Initialize the GPIO with a Configuration struct, and explicit pin */
+    void Init(Pin p, const Config &cfg);
+
+    /** @brief Explicity initialize all configuration for the GPIO */
+    void Init(Pin   p,
+              Mode  m  = Mode::INPUT,
+              Pull  pu = Pull::NOPULL,
+              Speed sp = Speed::LOW);
+
+    /** @brief Deinitializes the GPIO pin */
+    void DeInit();
+
+    /** @brief Reads the state of the GPIO.
+     *  If a GPIO is configured in Config::Mode::ANALOG, this function will 
+     *  always return false.
+     */
+    bool Read();
+
+    /** @brief Changes the state of the GPIO hardware when configured as an OUTPUT. 
+     *  @param state setting true writes an output HIGH, while setting false writes an output LOW.
+     */
+    void Write(bool state);
+
+    /** @brief flips the current state of the GPIO. 
+     *  If it was HIGH, it will go LOW, and vice versa.
+     */
+    void Toggle();
+
+    /** Return a reference to the internal Config struct */
+    Config &GetConfig() { return cfg_; }
+
+  private:
+    /** This will internally be cast to the 
+     *  STM32H7 GPIO_Typedef* type, which 
+     *  is just the base address to the
+     *  specified GPIO register. 
+     * 
+     *  This prevents us needing to have an internal
+     *  Impl class just to store the GPIO_Typedef* 
+     */
+    uint32_t *GetGPIOBaseRegister();
+
+    /** Internal copy of the Configuration of the given pin */
+    Config cfg_;
+
+    /** Internal pointer to base address of relavent GPIO register */
+    uint32_t *port_base_addr_;
+};
+
+} // namespace daisy
+
+#endif
+
+/** Old Style C API for GPIO is staying in place for a 
+ *  few versions to support backwards compatibility.
+ * 
+ *  This should not be used for anything new.
+ */
+
 #ifdef __cplusplus
 extern "C"
 {
