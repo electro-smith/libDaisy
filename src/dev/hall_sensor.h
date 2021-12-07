f674566be8dd2c5219cd2b1a0879f292979d999c
beserge <bensergentanis@gmail.com>
2021-12-07 13:30:02 -0800

    Finish the init process

--- src/dev/hall_sensor.h
+++ src/dev/hall_sensor.h
@@ -48,13 +48,13 @@ class HallSensor
         Config()
         {
             // These defaults are for the US5881
-            periph = Peripheral::TIM_2;
-            pin_   = {DSY_GPIOX, 0};
+            periph = Peripheral::TIM_3;
+            pin_   = {DSY_GPIOA, 8};
 
             polarity  = TIM_ICPOLARITY_RISING; // rising, falling, bothedge
             prescaler = TIM_ICPSC_DIV1;        // 1, 2, 4, 8
-            filter    = 0x0;                   // 0x0 -> 0xF
-            commutation_delay = 0x0000;        // 0x0000 -> 0xFFFF
+            filter    = 0x05;           // 0x0 -> 0xF basically the debounce
+            commutation_delay = 0x0000; // 0x0000 -> 0xFFFF
         }
     };
 
@@ -73,6 +73,8 @@ class HallSensor
         hall_conf_.Commutation_Delay = config_.commutation_delay;
 
         HAL_TIMEx_HallSensor_Init(&hall_, &hall_conf_);
+
+        StartBlockingRead(); // blocking read by default
     }
 
     void StartBlockingRead()
@@ -83,6 +85,8 @@ class HallSensor
         HAL_TIMEx_HallSensor_Start(&hall_);
     }
 
+    void StopBlockingRead() { HAL_TIMEx_HallSensor_Stop(&hall_); }
+
     uint8_t GetCount() { return hall_.Instance->CNT; }
 
     void InitPins()
@@ -91,7 +95,7 @@ class HallSensor
         GPIO_TypeDef*    port;
 
         GPIO_InitStruct.Mode  = GPIO_MODE_INPUT; // is this right?
-        GPIO_InitStruct.Pull  = GPIO_NOPULL;
+        GPIO_InitStruct.Pull  = GPIO_PULLUP;
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW; // what about this?
         switch(config_.periph)
         {
@@ -119,6 +123,7 @@ class HallSensor
         port                = dsy_hal_map_get_port(&config_.pin_);
         GPIO_InitStruct.Pin = dsy_hal_map_get_pin(&config_.pin_);
         HAL_GPIO_Init(port, &GPIO_InitStruct);
+        dsy_hal_map_gpio_clk_enable(config_.pin_.port);
     }
 
   private:
@@ -131,39 +136,30 @@ class HallSensor
 
 extern "C"
 {
-    void PinInit() {}
-
     void HAL_TIMEx_HallSensor_MspInit(TIM_HandleTypeDef* htim)
     {
-        PinInit();
         if(htim->Instance == TIM1)
         {
-            // __HAL_RCC_GPIOx_CLK_ENABLE(); // which ports go with which timers?
             __HAL_RCC_TIM1_CLK_ENABLE();
         }
         if(htim->Instance == TIM2)
         {
-            // __HAL_RCC_GPIOx_CLK_ENABLE();
             __HAL_RCC_TIM2_CLK_ENABLE();
         }
         else if(htim->Instance == TIM3)
         {
-            // __HAL_RCC_GPIOx_CLK_ENABLE();
             __HAL_RCC_TIM3_CLK_ENABLE();
         }
         else if(htim->Instance == TIM4)
         {
-            // __HAL_RCC_GPIOx_CLK_ENABLE();
             __HAL_RCC_TIM4_CLK_ENABLE();
         }
         else if(htim->Instance == TIM5)
         {
-            // __HAL_RCC_GPIOx_CLK_ENABLE();
             __HAL_RCC_TIM5_CLK_ENABLE();
         }
         else if(htim->Instance == TIM6)
         {
-            // __HAL_RCC_GPIOx_CLK_ENABLE();
             __HAL_RCC_TIM6_CLK_ENABLE();
         }
     }
