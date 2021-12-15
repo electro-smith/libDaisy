854387fe92b3ed54fb33da11b19d4f87b752077c
beserge <bensergentanis@gmail.com>
2021-12-15 16:55:39 -0500

    Update to new Pin

--- src/dev/vl53l1x.h
+++ src/dev/vl53l1x.h
@@ -64,8 +64,8 @@ class Vl53l1xI2CTransport
     {
         I2CHandle::Config::Peripheral periph;
         I2CHandle::Config::Speed      speed;
-        dsy_gpio_pin                  scl;
-        dsy_gpio_pin                  sda;
+        Pin                           scl;
+        Pin                           sda;
 
         uint8_t dev_addr;
 
@@ -74,8 +74,8 @@ class Vl53l1xI2CTransport
             periph = I2CHandle::Config::Peripheral::I2C_1;
             speed  = I2CHandle::Config::Speed::I2C_400KHZ;
 
-            scl = {DSY_GPIOB, 8};
-            sda = {DSY_GPIOB, 9};
+            scl = Pin(PORTB, 8);
+            sda = Pin(PORTB, 9);
 
             dev_addr = DEFAULT_DEVICE_ADDRESS;
         }
