9bd318aa84b77ff55ad075c3b84c1486938548eb
stephenhensley <stephenhensley@users.noreply.github.com>
2022-04-13 15:42:43 +0000

    deploy: 0013f723948c919c364e0aff7effacb148a9065b

--- group__controls.js
+++ group__controls.js
@@ -27,7 +27,7 @@ var group__controls =
     [ "daisy::GateIn", "classdaisy_1_1_gate_in.html", [
       [ "GateIn", "classdaisy_1_1_gate_in.html#a0ee9c8b8015845e9ac290f1de287ce03", null ],
       [ "~GateIn", "classdaisy_1_1_gate_in.html#af38fd662789d9d746f184703a2998f80", null ],
-      [ "Init", "classdaisy_1_1_gate_in.html#ab8f9cff6bcb78ec631cfa0d026c7f3b7", null ],
+      [ "Init", "classdaisy_1_1_gate_in.html#af9c75a81b23b1b268ca2eeeadd004c0c", null ],
       [ "State", "classdaisy_1_1_gate_in.html#a08f75c6621307249de3107df96cfab2d", null ],
       [ "Trig", "classdaisy_1_1_gate_in.html#a6a9bac9fe44985baa7097bd47a90a40f", null ]
     ] ],
