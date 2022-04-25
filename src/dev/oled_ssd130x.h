7db03e7917480dd18bcd460a185111a3110d3cf7
recursinging <recursinging+github@gmail.com>
2022-04-25 09:18:08 +0200

    Add entry to the changelog, fix formatting

--- src/dev/oled_ssd130x.h
+++ src/dev/oled_ssd130x.h
@@ -81,10 +81,11 @@ class SSD130x4WireSpiTransport
         void Defaults()
         {
             // SPI peripheral config
-            spi_config.periph    = SpiHandle::Config::Peripheral::SPI_1;
-            spi_config.mode      = SpiHandle::Config::Mode::MASTER;
-            spi_config.direction = SpiHandle::Config::Direction::TWO_LINES_TX_ONLY;
-            spi_config.datasize  = 8;
+            spi_config.periph = SpiHandle::Config::Peripheral::SPI_1;
+            spi_config.mode   = SpiHandle::Config::Mode::MASTER;
+            spi_config.direction
+                = SpiHandle::Config::Direction::TWO_LINES_TX_ONLY;
+            spi_config.datasize       = 8;
             spi_config.clock_polarity = SpiHandle::Config::ClockPolarity::LOW;
             spi_config.clock_phase    = SpiHandle::Config::ClockPhase::ONE_EDGE;
             spi_config.nss            = SpiHandle::Config::NSS::HARD_OUTPUT;
