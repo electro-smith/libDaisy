b255c86e71bf5ef5eb43d3c14f1ae28da03c6646
stephenhensley <stephen.p.hensley@gmail.com>
2022-12-07 17:03:03 -0800

    started putting together some stuff to drive the LED data via pulsewidth.

--- examples/PWM/AddressableLED/addressable-led.cpp
+++ examples/PWM/AddressableLED/addressable-led.cpp
@@ -4,6 +4,10 @@
  *  This can be common for things like Digital LEDs (WS8212), motor control, etc.
  * 
  *  For this we're going to use Daisy Seed pin D17 as TIM3 Channel 4 (unless it has issues because of the LED attached..)
+ * 
+ *  TODO: This won't work until this stuff is resolved:
+ *  * Hardcoding for TIM3Ch4 is in the libDaisy stuff. Needs to get updated so we can use TIM4 Ch1 for the devboard
+ *  * Some sort of stop/reset between transactions to prep LEDs for next signal
  */
 #include "daisy_seed.h"
 
@@ -14,6 +18,54 @@ DaisySeed                       hw;
 const size_t                    kOutBufferSize = 512;
 uint32_t DMA_BUFFER_MEM_SECTION outbuffer[kOutBufferSize];
 
+const int kOneTime  = 2;
+const int kZeroTime = 8;
+
+const int kNumLeds = 12;
+uint8_t   led_data[kNumLeds][3]; /**< RGB data */
+uint32_t DMA_BUFFER_MEM_SECTION
+    output_data[kNumLeds * 3
+                * 8]; /**< PWM lengths data, one "duration" per bit */
+
+/** buff expects that 8 elements are available for the one 8-bit color val*/
+static void populate_bits(uint8_t color_val, uint32_t *buff)
+{
+    for(int i = 0; i < 8; i++)
+    {
+        buff[i] = (color_val & i) > 0 ? kOneTime : kZeroTime;
+    }
+}
+
+static void fill_led_data()
+{
+    /** TODO fix these to be accurate for necessary timing */
+    const uint32_t one_time  = 2;
+    const uint32_t zero_time = 10;
+    for(int i = 0; i < kNumLeds; i++)
+    {
+        /** Grab G, R, B for filling bytes */
+        uint8_t g          = led_data[i][1];
+        uint8_t r          = led_data[i][0];
+        uint8_t b          = led_data[i][2];
+        auto    data_index = i * 3 * 8;
+        populate_bits(g, &output_data[data_index]);
+        populate_bits(r, &output_data[data_index + 8]);
+        populate_bits(b, &output_data[data_index + 16]);
+    }
+}
+
+static void set_led(int index, uint8_t r, uint8_t g, uint8_t b)
+{
+    led_data[index][0] = r;
+    led_data[index][1] = g;
+    led_data[index][2] = b;
+}
+
+static void set_led_f(int index, float r, float g, float b)
+{
+    set_led(index, r * 255, g * 255, b * 255);
+}
+
 int main(void)
 {
     /** Initialize hardware */
@@ -22,7 +74,7 @@ int main(void)
     /** Initialize timer */
     TimerHandle::Config tim_cfg;
     TimerHandle         timer;
-    tim_cfg.periph = TimerHandle::Config::Peripheral::TIM_3;
+    tim_cfg.periph = TimerHandle::Config::Peripheral::TIM_4;
     timer.Init(tim_cfg);
 
     /** Generate period for timer 
@@ -37,9 +89,9 @@ int main(void)
 
     TimChannel::Config chn_cfg;
     chn_cfg.tim  = &timer;
-    chn_cfg.chn  = TimChannel::Config::Channel::FOUR;
+    chn_cfg.chn  = TimChannel::Config::Channel::ONE;
     chn_cfg.mode = TimChannel::Config::Mode::PWM;
-    chn_cfg.pin  = seed::D17;
+    chn_cfg.pin  = seed::D13;
     TimChannel pwm;
     /** Fill Buffer */
     for(size_t i = 0; i < kOutBufferSize; i++)
