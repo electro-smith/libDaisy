182a907ca0444e8586155b74fddee28a85508cbe
beserge <beserge@users.noreply.github.com>
2024-02-21 21:01:12 +0000

    deploy: 77cc624fb2da480808ff10f94439cc0590289a73

--- structdaisy_1_1_audio_handle_1_1_config.js
+++ structdaisy_1_1_audio_handle_1_1_config.js
@@ -1,6 +1,5 @@
 var structdaisy_1_1_audio_handle_1_1_config =
 [
-    [ "Config", "structdaisy_1_1_audio_handle_1_1_config.html#a2543a29b73eb254e003f33502d88e991", null ],
     [ "blocksize", "structdaisy_1_1_audio_handle_1_1_config.html#a7e23631311ab3151daa7b4f5a49a897c", null ],
     [ "output_compensation", "structdaisy_1_1_audio_handle_1_1_config.html#a53848d6e1333445bcc8899039cb58a9a", null ],
     [ "postgain", "structdaisy_1_1_audio_handle_1_1_config.html#ab43119a5a246e3855730972314b416a4", null ],
