0deac435e061c8f8db050627ee6bd91fa615b289
beserge <beserge@users.noreply.github.com>
2024-03-06 21:04:28 +0000

    deploy: c85e2246e22a60778c0588dffa79bd525e86b362

--- oled__fonts_8h.js
+++ oled__fonts_8h.js
@@ -3,6 +3,10 @@ var oled__fonts_8h =
     [ "DSY_OLED_FONTS_H", "oled__fonts_8h.html#acd5a968545c0bd71b20fa678de249e0e", null ],
     [ "Font_11x18", "group__utility.html#gaa94e4e49025255db7783b4295468aedd", null ],
     [ "Font_16x26", "group__utility.html#ga72e53d9ac78461af8cae3a529842ea09", null ],
+    [ "Font_4x6", "group__utility.html#ga79366876080b45ab5c5d759134a221bb", null ],
+    [ "Font_4x8", "group__utility.html#ga00b1b9c997f61686416d23ead62e3409", null ],
+    [ "Font_5x8", "group__utility.html#ga2bb2cb54ceb4ac3a60dafc8c78e18391", null ],
+    [ "Font_6x7", "group__utility.html#ga869adb047433957c1201469d38c5d328", null ],
     [ "Font_6x8", "group__utility.html#ga4c55469540dd07f566c5a07d77495ad0", null ],
     [ "Font_7x10", "group__utility.html#gaf57a14b2dd6417892e306d9eb31a8470", null ]
 ];
--- search/variables_5.js
+++ search/variables_5.js
@@ -7,11 +7,15 @@ var searchData=
   ['fmt_4',['fmt',['../structdaisy_1_1_wm8731_1_1_config.html#a91edb01dd260a86a27fc80705a627435',1,'daisy::Wm8731::Config']]],
   ['font_5f11x18_5',['Font_11x18',['../group__utility.html#gaa94e4e49025255db7783b4295468aedd',1,'oled_fonts.h']]],
   ['font_5f16x26_6',['Font_16x26',['../group__utility.html#ga72e53d9ac78461af8cae3a529842ea09',1,'oled_fonts.h']]],
-  ['font_5f6x8_7',['Font_6x8',['../group__utility.html#ga4c55469540dd07f566c5a07d77495ad0',1,'oled_fonts.h']]],
-  ['font_5f7x10_8',['Font_7x10',['../group__utility.html#gaf57a14b2dd6417892e306d9eb31a8470',1,'oled_fonts.h']]],
-  ['fontheight_9',['FontHeight',['../struct_font_def.html#a4e3e31a3a441bc010a432564e5da887d',1,'FontDef']]],
-  ['fontwidth_10',['FontWidth',['../struct_font_def.html#a9e22d0bdbc21eadba6155f31d742f85f',1,'FontDef']]],
-  ['footswitch_5fled_11',['footswitch_led',['../classdaisy_1_1_daisy_petal.html#a0adb82d937f3b214214aa2370b12f75e',1,'daisy::DaisyPetal']]],
-  ['fs_12',['fs',['../structdaisy_1_1_sai_handle_1_1_config.html#aff0746f3a8ff80d4ad7763e5d4994b67',1,'daisy::SaiHandle::Config']]],
-  ['funcbttnid_13',['funcBttnId',['../structdaisy_1_1_u_i_1_1_special_control_ids.html#a7c847ff1dd0f5557428fa2c949af3872',1,'daisy::UI::SpecialControlIds']]]
+  ['font_5f4x6_7',['Font_4x6',['../group__utility.html#ga79366876080b45ab5c5d759134a221bb',1,'oled_fonts.h']]],
+  ['font_5f4x8_8',['Font_4x8',['../group__utility.html#ga00b1b9c997f61686416d23ead62e3409',1,'oled_fonts.h']]],
+  ['font_5f5x8_9',['Font_5x8',['../group__utility.html#ga2bb2cb54ceb4ac3a60dafc8c78e18391',1,'oled_fonts.h']]],
+  ['font_5f6x7_10',['Font_6x7',['../group__utility.html#ga869adb047433957c1201469d38c5d328',1,'oled_fonts.h']]],
+  ['font_5f6x8_11',['Font_6x8',['../group__utility.html#ga4c55469540dd07f566c5a07d77495ad0',1,'oled_fonts.h']]],
+  ['font_5f7x10_12',['Font_7x10',['../group__utility.html#gaf57a14b2dd6417892e306d9eb31a8470',1,'oled_fonts.h']]],
+  ['fontheight_13',['FontHeight',['../struct_font_def.html#a4e3e31a3a441bc010a432564e5da887d',1,'FontDef']]],
+  ['fontwidth_14',['FontWidth',['../struct_font_def.html#a9e22d0bdbc21eadba6155f31d742f85f',1,'FontDef']]],
+  ['footswitch_5fled_15',['footswitch_led',['../classdaisy_1_1_daisy_petal.html#a0adb82d937f3b214214aa2370b12f75e',1,'daisy::DaisyPetal']]],
+  ['fs_16',['fs',['../structdaisy_1_1_sai_handle_1_1_config.html#aff0746f3a8ff80d4ad7763e5d4994b67',1,'daisy::SaiHandle::Config']]],
+  ['funcbttnid_17',['funcBttnId',['../structdaisy_1_1_u_i_1_1_special_control_ids.html#a7c847ff1dd0f5557428fa2c949af3872',1,'daisy::UI::SpecialControlIds']]]
 ];
