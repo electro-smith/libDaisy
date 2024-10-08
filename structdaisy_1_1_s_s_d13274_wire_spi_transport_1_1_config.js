b9f7c0f31d302b9e59105ad5cc2279c1ddb58dd2
stephenhensley <stephenhensley@users.noreply.github.com>
2024-10-08 19:53:40 +0000

    deploy: 39aab667d4707f8226b6d304fcc1e23ddff20687

--- structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.js
+++ structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.js
@@ -2,8 +2,8 @@ var structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config =
 [
     [ "Config", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#afc19c695997efc70deb8b83b8f6b842f", null ],
     [ "Defaults", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#ab6559b28e83425102686d1fdad47dcc3", null ],
-    [ "dc", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#ad5f66624d90c7c99fa2c825d99bab364", null ],
+    [ "dc", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#ac657f9da876377fa98d0b2a653b8145c", null ],
     [ "pin_config", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#a5beab2d38ba5530f3124f7a6a8e31f1d", null ],
-    [ "reset", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#a887551e54f4ca1d7c40beee9096fdb7e", null ],
+    [ "reset", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#a305be0414d4bf137f54544a82d70462c", null ],
     [ "spi_config", "structdaisy_1_1_s_s_d13274_wire_spi_transport_1_1_config.html#a299c37171712b94b85330e39b2d8bb75", null ]
 ];
