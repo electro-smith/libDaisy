ba816ec5d71e609acaa422744e7064e3fe22fc2a
stephenhensley <stephenhensley@users.noreply.github.com>
2026-02-20 17:10:34 +0000

    deploy: 5ad4c36fb12f1b23d314812a0967e956197663c2

--- classdaisy_1_1_mpr121_i2_c_transport.js
+++ classdaisy_1_1_mpr121_i2_c_transport.js
@@ -4,6 +4,6 @@ var classdaisy_1_1_mpr121_i2_c_transport =
     [ "Mpr121I2CTransport", "classdaisy_1_1_mpr121_i2_c_transport.html#a38f985567ef23c8f1e03d829a6fb5aaf", null ],
     [ "~Mpr121I2CTransport", "classdaisy_1_1_mpr121_i2_c_transport.html#ae9488da639f811e781e6180cc188b426", null ],
     [ "Init", "classdaisy_1_1_mpr121_i2_c_transport.html#a4038e5bc1ba98d6a0968f43dc5c8259b", null ],
-    [ "Read", "classdaisy_1_1_mpr121_i2_c_transport.html#aa95902f995e64b0e2d44cfef43ef290e", null ],
-    [ "Write", "classdaisy_1_1_mpr121_i2_c_transport.html#afccd8ca8d49cb011e60d4848fd620aa5", null ]
+    [ "ReadDataAtAddress", "classdaisy_1_1_mpr121_i2_c_transport.html#a981fb177bc6b541172eda3d6e1fa97d3", null ],
+    [ "WriteDataAtAddress", "classdaisy_1_1_mpr121_i2_c_transport.html#a9f6bbd66dc44cdafc985a35c0c5fc85e", null ]
 ];
